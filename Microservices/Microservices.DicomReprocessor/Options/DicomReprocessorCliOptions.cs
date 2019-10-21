a912ecf16023b07632518ac412e446471382e8be
Ruairidh MacLeod <5160559+rkm@users.noreply.github.com>
2019-10-21 14:55:39 +0100

    Add DicomReprocessor source

---
+++ Microservices/Microservices.DicomReprocessor/Options/DicomReprocessorCliOptions.cs
@@ -0,0 +1,62 @@
+﻿
+using CommandLine;
+using CommandLine.Text;
+using Microservices.Common.Options;
+using ReusableLibraryCode.Annotations;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microservices.DicomReprocessor.Options
+{
+    public class DicomReprocessorCliOptions : CliOptions
+    {
+        [Option('c', "collection-name", Required = true, HelpText = "The collection to reprocess documents from")]
+        public string SourceCollection { get; set; }
+
+        [Option('q', "query-file", Required = false, HelpText = "The file to build the reprocessing query from")]
+        public string QueryFile { get; set; }
+
+        [Option("batch-size", Required = false, HelpText = "The batch size to query MongoDB with, if specified", Default = 0)]
+        public int MongoDbBatchSize { get; set; }
+
+        [Option("sleep-time", Required = false, HelpText = "TEMP: Sleep this number of ms between batches", Default = 0)]
+        public int SleepTime { get; set; }
+
+        /// <summary>
+        /// Routing key to republish messages with. Must not be null, otherwise the messages will end up back in MongoDB.
+        /// Must match the routing key of the binding to the queue you wish the messages to end up in.
+        /// </summary>
+        [Option("reprocessing-key", Required = false, HelpText = "Routing key to reprocess messages with. Do not change from default for most cases", Default = "reprocessed")]
+        public string ReprocessingRoutingKey { get; set; }
+
+        [Option("auto-run", Required = false, HelpText = "Automatically run the query without asking for confirmation", Default = false)]
+        public bool AutoRun { get; set; }
+
+
+        [Usage]
+        [UsedImplicitly]
+        public static IEnumerable<Example> Examples
+        {
+            get
+            {
+                yield return
+                    new Example("Normal Scenario", new DicomReprocessorCliOptions { SourceCollection = "image", MongoDbBatchSize = 123 });
+                yield return
+                    new Example("Normal Scenario", new DicomReprocessorCliOptions { SourceCollection = "image", MongoDbBatchSize = 123, ReprocessingRoutingKey = "test", AutoRun = true });
+            }
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            sb.Append("SourceCollection: " + SourceCollection);
+            sb.Append(", QueryFile: " + QueryFile);
+            sb.Append(", MongoDbBatchSize: " + MongoDbBatchSize);
+            sb.Append(", ReprocessingRoutingKey: " + ReprocessingRoutingKey);
+            sb.Append(", AutoRun: " + AutoRun);
+
+            return base.ToString() + ", " + sb;
+        }
+    }
+}
---
+++ Microservices/Microservices.DicomReprocessor/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using CommandLine;
+using Microservices.Common.Execution;
+using Microservices.Common.Options;
+using Microservices.DicomReprocessor.Execution;
+using Microservices.DicomReprocessor.Options;
+
+namespace Microservices.DicomReprocessor
+{
+    /// <summary>
+    /// Command line program to reprocess documents from MongoDb
+    /// and push them back onto the IdentifiableImageQueue for
+    /// reprocessing into the relational database.
+    /// </summary>
+    internal static class Program
+    {
+        private static int Main(string[] args)
+        {
+            return Parser.Default.ParseArguments<DicomReprocessorCliOptions>(args)
+                .MapResult(dicomReprocessorCliOptions =>
+                {
+                    GlobalOptions options = GlobalOptions.Load(dicomReprocessorCliOptions);
+
+                    var bootStrapper = new MicroserviceHostBootstrapper(() => new DicomReprocessorHost(options, dicomReprocessorCliOptions));
+                    return bootStrapper.Main();
+
+                }, err => -100);
+        }
+    }
+}
