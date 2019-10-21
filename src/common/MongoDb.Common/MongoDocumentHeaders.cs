622e499c7d1bd418883ed91c75e08c326ba164ea
Ruairidh MacLeod <5160559+rkm@users.noreply.github.com>
2019-10-21 20:18:33 +0100

    Add missing MongoDb.Common project

---
+++ src/common/MongoDb.Common/MongoDocumentHeaders.cs
@@ -0,0 +1,61 @@
+﻿
+using System;
+using Microservices.Common.Messages;
+using MongoDB.Bson;
+
+
+namespace Smi.MongoDB.Common
+{
+    public static class MongoDocumentHeaders
+    {
+        /// <summary>
+        /// Generate a header for an image document
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="header"></param>
+        /// <returns></returns>
+        public static BsonDocument ImageDocumentHeader(DicomFileMessage message, IMessageHeader header)
+        {
+            return new BsonDocument
+            {
+                { "DicomFilePath",               message.DicomFilePath },
+                { "NationalPACSAccessionNumber", message.NationalPACSAccessionNumber },
+                { "MessageHeader", new BsonDocument
+                    {
+                        { "MessageGuid", header.MessageGuid.ToString() },
+                        { "ProducerProcessID", header.ProducerProcessID },
+                        { "ProducerExecutableName", header.ProducerExecutableName },
+                        { "Parents", string.Join(MessageHeader.Splitter, header.Parents) },
+                        { "OriginalPublishTimestamp", header.OriginalPublishTimestamp }
+                    }}
+            };
+        }
+
+        /// <summary>
+        /// Generate a header for a series document
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public static BsonDocument SeriesDocumentHeader(SeriesMessage message)
+        {
+            return new BsonDocument
+            {
+                { "DirectoryPath",               message.DirectoryPath },
+                { "NationalPACSAccessionNumber", message.NationalPACSAccessionNumber },
+                { "ImagesInSeries",              message.ImagesInSeries }
+            };
+        }
+
+        public static IMessageHeader RebuildMessageHeader(BsonDocument bsonDoc)
+        {
+            return new MessageHeader
+            {
+                MessageGuid = Guid.Parse(bsonDoc["MessageGuid"].AsString),
+                ProducerProcessID = bsonDoc["ProducerProcessID"].AsInt32,
+                ProducerExecutableName = bsonDoc["ProducerExecutableName"].AsString,
+                Parents = MessageHeader.GetGuidArray(bsonDoc["Parents"].AsString),
+                OriginalPublishTimestamp = bsonDoc["OriginalPublishTimestamp"].AsInt64
+            };
+        }
+    }
+}
