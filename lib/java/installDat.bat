a614502d3e5f548a2f96992f798373bbdbd6c5b3
Ruairidh MacLeod <5160559+rkm@users.noreply.github.com>
2019-10-22 19:19:42 +0100

    Fix links in CTP/DAT install script

--- lib/java/installDat.bat
+++ lib/java/installDat.bat
@@ -1,15 +1,16 @@
 
 :: install DATs dependencies to your local repo
-call mvn install:install-file -Dfile=".\lib\CTP.jar" -DgroupId="dat" -DartifactId="ctp" -Dversion="1.0" -Dpackaging="jar" -DgeneratePom="true"
-call mvn install:install-file -Dfile=".\lib\dcm4che.jar" -DgroupId="dat" -DartifactId="dcm4che" -Dversion="1.0" -Dpackaging="jar" -DgeneratePom="true"
-call mvn install:install-file -Dfile=".\lib\util.jar" -DgroupId="dat" -DartifactId="util" -Dversion="1.0" -Dpackaging="jar" -DgeneratePom="true"
-call mvn install:install-file -Dfile=".\lib\log4j.jar" -DgroupId="dat" -DartifactId="log4j" -Dversion="1.0" -Dpackaging="jar" -DgeneratePom="true"
-call mvn install:install-file -Dfile=".\lib\pixelmed_codec.jar" -DgroupId="dat" -DartifactId="pixelmed_codec" -Dversion="1.0" -Dpackaging="jar" -DgeneratePom="true"
-call mvn install:install-file -Dfile=".\lib\dcm4che-imageio-rle-2.0.25.jar" -DgroupId="dat" -DartifactId="dcm4che-imageio-rle-2.0.25" -Dversion="1.0" -Dpackaging="jar" -DgeneratePom="true"
+call mvn install:install-file -Dfile=".\CTP.jar" -DgroupId="dat" -DartifactId="ctp" -Dversion="1.0" -Dpackaging="jar" -DgeneratePom="true"
+call mvn install:install-file -Dfile=".\dcm4che.jar" -DgroupId="dat" -DartifactId="dcm4che" -Dversion="1.0" -Dpackaging="jar" -DgeneratePom="true"
+call mvn install:install-file -Dfile=".\util.jar" -DgroupId="dat" -DartifactId="util" -Dversion="1.0" -Dpackaging="jar" -DgeneratePom="true"
+call mvn install:install-file -Dfile=".\log4j.jar" -DgroupId="dat" -DartifactId="log4j" -Dversion="1.0" -Dpackaging="jar" -DgeneratePom="true"
+call mvn install:install-file -Dfile=".\pixelmed_codec.jar" -DgroupId="dat" -DartifactId="pixelmed_codec" -Dversion="1.0" -Dpackaging="jar" -DgeneratePom="true"
+call mvn install:install-file -Dfile=".\dcm4che-imageio-rle-2.0.25.jar" -DgroupId="dat" -DartifactId="dcm4che-imageio-rle-2.0.25" -Dversion="1.0" -Dpackaging="jar" -DgeneratePom="true"
 
 :: Install some required CTP dependencies
-call mvn install:install-file -Dfile=".\lib\clibwrapper_jiio.jar" -DgroupId="dat" -DartifactId="clibwrapper_jiio" -Dversion="1.1" -Dpackaging="jar" -DgeneratePom="true"
-call mvn install:install-file -Dfile=".\lib\jai_imageio.jar" -DgroupId="dat" -DartifactId="jai_imageio" -Dversion="1.1" -Dpackaging="jar" -DgeneratePom="true"
+call mvn install:install-file -Dfile=".\clibwrapper_jiio.jar" -DgroupId="dat" -DartifactId="clibwrapper_jiio" -Dversion="1.1" -Dpackaging="jar" -DgeneratePom="true"
+call mvn install:install-file -Dfile=".\jai_imageio.jar" -DgroupId="dat" -DartifactId="jai_imageio" -Dversion="1.1" -Dpackaging="jar" -DgeneratePom="true"
 
 :: Install DAT, giving it the pom we have generated
-call mvn install:install-file -Dfile=".\lib\DAT.jar" -DpomFile=".\lib\pom.xml"
+call mvn install:install-file -Dfile=".\DAT.jar" -DpomFile=".\pom.xml"
+
