c3f8f80d1abc9416345b1aa6ed4f332f26b360c1
Ruairidh MacLeod <5160559+rkm@users.noreply.github.com>
2019-11-06 14:53:07 +0000

    Update packages

--- tests/common/Smi.Common.Tests/NuspecIsCorrectTests.cs
+++ tests/common/Smi.Common.Tests/NuspecIsCorrectTests.cs
@@ -1,13 +1,9 @@
-﻿// Copyright (c) The University of Dundee 2018-2019
-// This file is part of the Research Data Management Platform (RDMP).
-// RDMP is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-// RDMP is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-// You should have received a copy of the GNU General Public License along with RDMP. If not, see <https://www.gnu.org/licenses/>.
 
+using NUnit.Framework;
+using System;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
-using NUnit.Framework;
 
 
 namespace Smi.Common.Tests
@@ -31,17 +27,6 @@ namespace Smi.Common.Tests
         [TestCase("../../../../../../Microservices/Microservices.IdentifierMapper/Microservices.IdentifierMapper.csproj", null, "../../../../../../Packages.md")]
         [TestCase("../../../../../../Microservices/Microservices.MongoDBPopulator/Microservices.MongoDBPopulator.csproj", null, "../../../../../../Packages.md")]
         [TestCase("../../../../../../Microservices/Microservices.ProcessDirectory/Microservices.ProcessDirectory.csproj", null, "../../../../../../Packages.md")]
-
-        [TestCase("../../../../../../Applications/DicomFileTester/DicomFileTester.csproj", null, "../../../../../../Packages.md")]
-        [TestCase("../../../../../../Applications/DicomRepopulator/DicomRepopulator.csproj", null, "../../../../../../Packages.md")]
-        [TestCase("../../../../../../Applications/TemplateBuilder/TemplateBuilder.csproj", null, "../../../../../../Packages.md")]
-
-        [TestCase("../../../../../../Benchmarks/MongoDb/MongoDbReadBenchmark/MongoDbReadBenchmark.csproj", null, "../../../../../../Packages.md")]
-        [TestCase("../../../../../../Benchmarks/RabbitMq/RabbitMqProducerBenchmark/RabbitMqProducerBenchmark.csproj", null, "../../../../../../Packages.md")]
-        [TestCase("../../../../../../Benchmarks/RabbitMq/RabbitMqPublishBenchmark/RabbitMqPublishBenchmark.csproj", null, "../../../../../../Packages.md")]
-        [TestCase("../../../../../../Benchmarks/DicomReprocessorBenchmarks/DicomReprocessorBenchmarks.csproj", null, "../../../../../../Packages.md")]
-        [TestCase("../../../../../../Benchmarks/DicomTagReaderBenchmarks/DicomTagReaderBenchmarks.csproj", null, "../../../../../../Packages.md")]
-
         [TestCase("../../../../../../Reusable/Smi.MongoDB.Common/Smi.MongoDB.Common.csproj", null, "../../../../../../Packages.md")]
 
         public void TestDependencyCorrect(string csproj, string nuspec, string packagesMarkdown)
