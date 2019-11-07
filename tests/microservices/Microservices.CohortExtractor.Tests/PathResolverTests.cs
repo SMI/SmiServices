640ee92c5369b987a8af49e47976e1bb1e92dd3a
Ruairidh MacLeod <5160559+rkm@users.noreply.github.com>
2019-11-07 12:33:29 +0000

    PathResolverTests: Fix paths for Linux

--- tests/microservices/Microservices.CohortExtractor.Tests/PathResolverTests.cs
+++ tests/microservices/Microservices.CohortExtractor.Tests/PathResolverTests.cs
@@ -10,21 +10,15 @@ namespace Microservices.CohortExtractor.Tests
     [TestFixture]
     public class PathResolverTests
     {
-        private const string FilePath = @"2018\01\01\ABCD1234\testDicom.dcm";
+        private const string FilePath = "2018/01/01/AAAA/testDicom.dcm";
         private const string SeriesId = "1.2.3.4";
-
-        private readonly ExtractionRequestMessage _requestMessage = new ExtractionRequestMessage
-        {
-            ExtractionDirectory = @"1718-0316e\testExtract"
-        };
-
 
         [Test]
         public void TestDefaultPathResolver()
         {
             var collection = new ExtractImageCollection(SeriesId, SeriesId, new HashSet<string>(new[] { FilePath }));
 
-            Assert.AreEqual(@"testDicom-an.dcm", new DefaultProjectPathResolver().GetOutputPath(FilePath, collection));
+            Assert.AreEqual("testDicom-an.dcm", new DefaultProjectPathResolver().GetOutputPath(FilePath, collection));
         }
 
         [Test]
@@ -32,7 +26,7 @@ namespace Microservices.CohortExtractor.Tests
         {
             var collection = new ExtractImageCollection(SeriesId, SeriesId, new HashSet<string>(new[] { FilePath }));
 
-            Assert.AreEqual(@"1.2.3.4\testDicom-an.dcm", new SeriesKeyPathResolver().GetOutputPath(FilePath, collection));
+            Assert.AreEqual("1.2.3.4/testDicom-an.dcm", new SeriesKeyPathResolver().GetOutputPath(FilePath, collection));
         }
     }
 }
