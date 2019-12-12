c68bf779a08588d5f0612069492ef7bea1d44cac
Ruairidh MacLeod <5160559+rkm@users.noreply.github.com>
2019-12-12 18:54:14 +0000

    DicomReprocessor: Add test for invalid CLI argument & tidy options

--- src/common/Smi.Common/Options/CliOptions.cs
+++ src/common/Smi.Common/Options/CliOptions.cs
@@ -5,10 +5,21 @@ namespace Smi.Common.Options
 {
     public class CliOptions
     {
-        [Option('y', "yaml-file", Default = "default.yaml", HelpText = "Name of the yaml config file to load")]
+        [Option(
+            'y',
+            "yaml-file",
+            Default = "default.yaml",
+            Required = false,
+            HelpText = "[Optional] Name of the yaml config file to load"
+        )]
         public string YamlFile { get; set; }
 
-        [Option("trace-logging", Default = true, HelpText = "Enable trace logging")]
+        [Option(
+            "trace-logging",
+            Default = true,
+            Required = false,
+            HelpText = "[Optional] Enable trace logging"
+        )]
         public bool TraceLogging { get; set; }
     }
 }
