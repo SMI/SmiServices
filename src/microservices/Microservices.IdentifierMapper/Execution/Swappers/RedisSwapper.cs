804edbe6e0ebb497c95cd52cb3552c42c72f5f18
Thomas Nind <TZNind@dundee.ac.uk>
2020-01-08 12:16:22 +0000

    Added RedisSwapper
    - Caches answers to the wrapped swapper in Redis database

---
+++ src/microservices/Microservices.IdentifierMapper/Execution/Swappers/RedisSwapper.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Smi.Common.Options;
+using StackExchange.Redis;
+
+namespace Microservices.IdentifierMapper.Execution.Swappers
+{
+    /// <summary>
+    /// A swapper that wraps <see cref="TableLookupWithGuidFallbackSwapper"/> and uses a Redis database (on localhost)
+    /// to store cached values
+    /// </summary>
+    public class RedisSwapper : SwapIdentifiers,IDisposable
+    {
+        private readonly ConnectionMultiplexer _redis;
+        private TableLookupWithGuidFallbackSwapper _hostedSwapper;
+
+        public const string RedistServer = "localhost";
+
+        public RedisSwapper()
+        {
+            _redis = ConnectionMultiplexer.Connect(RedistServer);
+            _hostedSwapper = new TableLookupWithGuidFallbackSwapper();
+        }
+        public override void Setup(IMappingTableOptions mappingTableOptions)
+        {
+            _hostedSwapper.Setup(mappingTableOptions);
+        }
+
+        public override string GetSubstitutionFor(string toSwap, out string reason)
+        {
+            IDatabase db = _redis.GetDatabase();
+            
+            var val = db.StringGet(toSwap);
+
+            if (val.HasValue)
+            {
+                reason = null;
+                CacheHit++;
+                Success++;
+                return val.ToString();
+            }
+
+            CacheMiss++;
+            
+            var output = _hostedSwapper.GetSubstitutionFor(toSwap, out reason);
+            db.StringSet(toSwap, output, null, When.NotExists);
+
+            Success++;
+            return output;
+        }
+
+
+        public override void ClearCache()
+        {
+            using(var admin = ConnectionMultiplexer.Connect(RedistServer +",allowAdmin=true"))
+                foreach (var server in admin.GetEndPoints().Select(e=> admin.GetServer(e)))
+                    server.FlushAllDatabases();
+            
+            _hostedSwapper.ClearCache();
+        }
+
+        public void Dispose()
+        {
+            _redis?.Dispose();
+        }
+    }
+}
---
+++ tests/microservices/Microservices.IdentifierMapper.Tests/RedisSwapperTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using FAnsi;
+using FAnsi.Discovery;
+using Microservices.IdentifierMapper.Execution.Swappers;
+using NUnit.Framework;
+using Smi.Common.Options;
+using Smi.Common.Tests;
+using Tests.Common;
+
+namespace Microservices.IdentifierMapper.Tests
+{
+    [RequiresRelationalDb(DatabaseType.MicrosoftSQLServer)]
+    [RequiresRelationalDb(DatabaseType.MySql)]
+    class RedisSwapperTests : DatabaseTests
+    {
+        [TestCase(DatabaseType.MySql)]
+        [TestCase(DatabaseType.MicrosoftSQLServer)]
+        public void Test_Redist_CacheUsage(DatabaseType dbType)
+        {
+            var db = GetCleanedServer(dbType);
+
+            DiscoveredTable map;
+            
+            using (var dt = new DataTable())
+            {
+                dt.Columns.Add("CHI");
+                dt.Columns.Add("ECHI");
+
+                dt.Rows.Add("0101010101", "0A0A0A0A0A");
+                map = db.CreateTable("Map",dt);
+            }
+
+            var options = new IdentifierMapperOptions()
+            {
+                MappingTableName = map.GetFullyQualifiedName(),
+                MappingConnectionString = db.Server.Builder.ConnectionString,
+                SwapColumnName = "CHI",
+                ReplacementColumnName = "ECHI",
+                MappingDatabaseType = db.Server.DatabaseType
+            };
+
+            var swapper = new RedisSwapper();
+            swapper.Setup(options);
+            swapper.ClearCache();
+
+            //hit on the lookup table
+            string answer = swapper.GetSubstitutionFor("0101010101",out string reason);
+            Assert.AreEqual("0A0A0A0A0A",answer);
+            Assert.IsNull(reason);
+
+            //hit didn't come from Redis
+            Assert.AreEqual(0,swapper.CacheHit);
+            Assert.AreEqual(1,swapper.Success);
+
+            
+            //hit from Redis
+            string answer2 = swapper.GetSubstitutionFor("0101010101",out string reason2);
+            Assert.AreEqual("0A0A0A0A0A",answer);
+            Assert.IsNull(reason);
+            
+            //hit must come from Redis
+            Assert.AreEqual(1,swapper.CacheHit);
+            Assert.AreEqual(2,swapper.Success);
+
+
+        }
+    }
+}
