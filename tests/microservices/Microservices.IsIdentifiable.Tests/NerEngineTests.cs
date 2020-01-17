b1e5660e838163a396551dfd5321898fa9704a55
Thomas Nind <TZNind@dundee.ac.uk>
2020-01-17 15:32:48 +0000

    Added testing of IsIdentifiable into travis-ci

--- tests/microservices/Microservices.IsIdentifiable.Tests/NerEngineTests.cs
+++ tests/microservices/Microservices.IsIdentifiable.Tests/NerEngineTests.cs
@@ -8,24 +8,24 @@ namespace Microservices.IsIdentifiable.Tests
 {
     class NerEngineTests
     {
-        public const string ClassifierPath = @"C:\temp\stanford-ner-2017-06-09\classifiers\english.all.3class.distsim.crf.ser.gz";
+        public const string ClassifierPath = @"../../../../../../../stanford-ner/stanford-ner-2016-10-31/classifiers/english.all.3class.distsim.crf.ser.gz";
 
         [Test]
         public void NerEngine_TestWhitelist()
         {
-            string f = ClassifierPath;
+            var f = new FileInfo(ClassifierPath);
 
-            if (!File.Exists(f))
+            if (!f.Exists)
                 Assert.Inconclusive("File did not exist " + f);
 
             var whitelist = Mock.Of<IWhitelistSource>(
                 w => w.GetWhitelist() == new[] {"Good afternoon Rajat Raina, how are you today?"});
 
-            var engine = new NerEngine(f,whitelist);
+            var engine = new NerEngine(f.FullName,whitelist);
             
             Assert.IsEmpty(engine.MatchNames("Good afternoon Rajat Raina, how are you today?"));
 
-            engine = new NerEngine(f, null);
+            engine = new NerEngine(f.FullName, null);
 
             Assert.IsNotEmpty(engine.MatchNames("Good afternoon Rajat Raina, how are you today?"));
         }
