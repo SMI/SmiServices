e03129847f562712ade14f2a65137aaca5cc1dd8
Thomas Nind <TZNind@dundee.ac.uk>
2020-01-24 13:30:05 +0000

    Added equality members

--- src/common/Smi.Common/Messages/Extraction/IsIdentifiableMessage.cs
+++ src/common/Smi.Common/Messages/Extraction/IsIdentifiableMessage.cs
@@ -3,7 +3,7 @@ using Newtonsoft.Json;
 
 namespace Smi.Common.Messages.Extraction
 {
-    public class IsIdentifiableMessage : ExtractMessage,IFileReferenceMessage,IMessage
+    public class IsIdentifiableMessage : ExtractMessage,IFileReferenceMessage,IMessage>,IEquatable<IsIdentifiableMessage>
     {
         public bool IsIdentifiable { get; set; }
 
@@ -41,6 +41,37 @@ namespace Smi.Common.Messages.Extraction
             DicomFilePath = request.DicomFilePath;
             AnonymisedFileName = request.AnonymisedFileName;
         }
+
+        #region Equality Members
+
+        public bool Equals(IsIdentifiableMessage other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return base.Equals(other) && IsIdentifiable == other.IsIdentifiable && DicomFilePath == other.DicomFilePath && AnonymisedFileName == other.AnonymisedFileName;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((IsIdentifiableMessage) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hashCode = base.GetHashCode();
+                hashCode = (hashCode * 397) ^ IsIdentifiable.GetHashCode();
+                hashCode = (hashCode * 397) ^ (DicomFilePath != null ? DicomFilePath.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (AnonymisedFileName != null ? AnonymisedFileName.GetHashCode() : 0);
+                return hashCode;
+            }
+        }
+
+        #endregion
 
     }
 }
