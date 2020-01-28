15c13307542d47face0dd11da975f013f43feb44
abrooks <abrooks>
2020-01-28 14:40:21 +0000

    Use ImageMagick to pre-process pixels for better Tesseract results

--- src/microservices/Microservices.IsIdentifiable/Runners/DicomFileRunner.cs
+++ src/microservices/Microservices.IsIdentifiable/Runners/DicomFileRunner.cs
@@ -16,6 +16,7 @@ using Microservices.IsIdentifiable.Reporting.Reports;
 using NLog;
 using Tesseract;
 using ImageFormat = System.Drawing.Imaging.ImageFormat;
+using ImageMagick; // dotnet add package Magick.NET-Q16-AnyCPU
 
 namespace Microservices.IsIdentifiable.Runners
 {
@@ -187,7 +188,30 @@ namespace Microservices.IsIdentifiable.Runners
                         {
                             Process(targetBmp, oldBmp.PixelFormat, targetBmp.PixelFormat, fi, dicomFile, sopID, studyID, seriesID, modality, imageType);
 
-                            //if user wants to rotate the image 90, 180 and 270 degress
+                            // Need to threshold and possibly negate the image for best results
+                            // Magick.NET won't read from Bitmap directly in .net core so go via MemoryStream
+                            MemoryStream memStreamIn = new MemoryStream();
+                            targetBmp.Save(memStreamIn, ImageFormat.Bmp);
+                            MagickImage mi = new MagickImage();
+                            memStreamIn.Position = 0;
+                            mi.Read(memStreamIn);
+                            // Threshold the image to monochrome using a window size of 25 square
+                            // The size 25 was determined empirically based on real images (could be larger, less effective if smaller)
+                            mi.AdaptiveThreshold(25, 25);
+                            // Write out to memory, can't reuse memStreamIn here as it breaks
+                            MemoryStream memStreamOut = new MemoryStream();
+                            mi.Write(memStreamOut);
+                            memStreamOut.Position = 0;
+                            Process(memStreamOut, oldBmp.PixelFormat, targetBmp.PixelFormat, fi, dicomFile, sopID, studyID, seriesID, modality, imageType);
+                            // Tesseract only works with black text on white background so run again negated
+                            mi.Negate();
+                            memStreamOut.Position = 0;
+                            mi.Write(memStreamOut);
+                            memStreamOut.Position = 0;
+                            Process(memStreamOut, oldBmp.PixelFormat, targetBmp.PixelFormat, fi, dicomFile, sopID, studyID, seriesID, modality, imageType);
+
+                            //if user wants to rotate the image 90, 180 and 270 degrees
+                            // XXX this is done after negation, maybe needs to be done with and without negation?
                             if (_opts.Rotate)
                                 for (int i = 0; i < 3; i++)
                                 {
@@ -208,12 +232,19 @@ namespace Microservices.IsIdentifiable.Runners
 
         private void Process(Bitmap targetBmp, PixelFormat pixelFormat, PixelFormat processedPixelFormat, FileInfo fi, DicomFile dicomFile, string sopID, string studyID, string seriesID, string modality, string[] imageType, int rotationIfAny = 0)
         {
-            float meanConfidence;
-            string text;
-
             using (var ms = new MemoryStream())
             {
                 targetBmp.Save(ms,ImageFormat.Bmp);
+                Process(ms, pixelFormat, processedPixelFormat, fi, dicomFile, sopID, studyID, seriesID, modality, imageType, rotationIfAny);
+            }
+        }
+
+        private void Process(MemoryStream ms, PixelFormat pixelFormat, PixelFormat processedPixelFormat, FileInfo fi, DicomFile dicomFile, string sopID, string studyID, string seriesID, string modality, string[] imageType, int rotationIfAny = 0)
+        {
+            float meanConfidence;
+            string text;
+
+            {
                 var bytes = ms.ToArray();
 
                 // targetBmp is now in the desired format.
