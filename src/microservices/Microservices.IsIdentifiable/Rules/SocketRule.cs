2db2b2001928f2404b047b57195f70b0ddcac304
tznind <t.z.nind@dundee.ac.uk>
2020-01-28 12:56:48 +0000

    Added socket based ICustomRule classifier

---
+++ src/microservices/Microservices.IsIdentifiable/Rules/SocketRule.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+using Microservices.IsIdentifiable.Failures;
+
+namespace Microservices.IsIdentifiable.Rules
+{
+    public class SocketRule : ICustomRule,IDisposable
+    {
+        private readonly string _hostname;
+        private readonly int _port;
+        private TcpClient _tcp;
+        private NetworkStream _stream;
+
+        public SocketRule(string hostname, int port)
+        {
+            _hostname = hostname;
+            _port = port;
+        }
+
+        public RuleAction Apply(string fieldName, string fieldValue, out FailureClassification classification,
+            out int offset)
+        {
+            if (_stream == null)
+            {
+                _tcp = new TcpClient(_hostname, _port);
+                _stream = _tcp.GetStream();
+            }
+
+            // Translate the passed message into ASCII and store it as a Byte array.
+            byte[] data = Encoding.UTF8.GetBytes(fieldValue);         
+
+            // Send the message to the connected TcpServer. 
+            _stream.Write(data, 0, data.Length);
+   
+            // Receive the TcpServer.response.
+    
+            // Buffer to store the response bytes.
+            data = new byte[256];
+
+            // String to store the response UTF8 representation.
+
+            // Read the first batch of the TcpServer response bytes.
+            int bytes = _stream.Read(data, 0, data.Length);
+            var responseData = System.Text.Encoding.UTF8.GetString(data, 0, bytes);
+
+            return HandleResponse(responseData, out classification, out offset);
+        }
+
+        public RuleAction HandleResponse(string responseData, out FailureClassification classification, out int offset)
+        {
+            if (string.Equals(responseData, "\0"))
+            {
+                classification = FailureClassification.None;
+                offset = -1;
+                return RuleAction.None;
+            }
+                
+            var result = responseData.Split("\0",StringSplitOptions.RemoveEmptyEntries);
+            
+            if(result.Length != 2)
+                throw new Exception($"Unexpected number of tokens in response from TCP client (expected '{2}' but got '{result.Length}').  Full message was '{responseData}' (expected <classification><offset> or <null terminator>)");
+
+            object c;
+            if (!Enum.TryParse(typeof(FailureClassification), result[0],true, out c))
+                throw new Exception($"Could not parse TCP client classification '{result[0]}' (expected a member of Enum FailureClassification)");
+            classification = (FailureClassification)c;
+
+            if(!int.TryParse(result[1],out offset))
+                throw new Exception($"Failed to parse offset from TCP client response.  Response was '{result[1]}' (expected int)");
+
+            return RuleAction.Report;
+        }
+
+        public void Dispose()
+        {
+            _tcp?.Dispose();
+            _stream?.Dispose();
+        }
+    }
+}
---
+++ tests/microservices/Microservices.IsIdentifiable.Tests/SocketRuleTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microservices.IsIdentifiable.Failures;
+using Microservices.IsIdentifiable.Rules;
+using NUnit.Framework;
+
+namespace Microservices.IsIdentifiable.Tests
+{
+    class SocketRuleTests
+    {
+        [Test]
+        public void TestSocket_NegativeResponse()
+        {
+            using (var socketRule = new SocketRule("localhost",123))
+            {
+                Assert.AreEqual(RuleAction.None,socketRule.HandleResponse("\0", out FailureClassification c, out int offset));
+                Assert.AreEqual(FailureClassification.None,c);
+                Assert.AreEqual(-1,offset);
+            }
+        }
+        
+        [Test]
+        public void TestSocket_PositiveResponse()
+        {
+            using (var socketRule = new SocketRule("localhost",123))
+            {
+                Assert.AreEqual(RuleAction.Report,socketRule.HandleResponse("Person\010\0", out FailureClassification c, out int offset));
+                Assert.AreEqual(FailureClassification.Person,c);
+                Assert.AreEqual(10,offset);
+            }
+        }
+                
+        [Test]
+        public void TestSocket_InvalidResponses()
+        {
+            using (var socketRule = new SocketRule("localhost",123))
+            {
+                var ex = Assert.Throws<Exception>(()=>socketRule.HandleResponse("Cadbury\010\0", out FailureClassification c, out int offset));
+                StringAssert.Contains("'Cadbury' (expected a member of Enum FailureClassification)",ex.Message);
+
+                ex = Assert.Throws<Exception>(()=>socketRule.HandleResponse("Person\0fish\0", out FailureClassification c, out int offset));
+                StringAssert.Contains("Response was 'fish' (expected int)",ex.Message);
+                
+                ex = Assert.Throws<Exception>(()=>socketRule.HandleResponse("Person\0", out FailureClassification c, out int offset));
+                StringAssert.Contains("Unexpected number of tokens in response from TCP client (expected '2' but got '1')",ex.Message);
+            }
+        }
+    }
+}
