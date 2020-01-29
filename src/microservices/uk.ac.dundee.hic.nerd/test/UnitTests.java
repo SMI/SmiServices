a5c00d342e1003cad7f60fdcf6aa8037923e7db3
James A Sutherland <j.a.sutherland@dundee.ac.uk>
2020-01-29 15:09:04 +0000

    Maven build, test, dependencies handling
    Get Maven fetching, testing and packaging successfully.
    Dummy test case only so far, still need to handle NER model file.

---
+++ src/microservices/uk.ac.dundee.hic.nerd/test/UnitTests.java
@@ -0,0 +1,10 @@
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class UnitTests {
+	@Test public void testClassifier() {
+		Assertions.assertEquals(1, 1);
+	}
+}
