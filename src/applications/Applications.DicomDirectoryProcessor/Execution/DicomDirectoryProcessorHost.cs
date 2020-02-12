6ae0f55231a4ad9074a3e38b11c08a8a0a2c1c94
2bPro <b.prodan95@gmail.com>
2020-02-12 14:30:47 +0000

    BP: Adding 'list' mode to DicomDirectoryProcessor

--- src/applications/Applications.DicomDirectoryProcessor/Execution/DicomDirectoryProcessorHost.cs
+++ src/applications/Applications.DicomDirectoryProcessor/Execution/DicomDirectoryProcessorHost.cs
@@ -27,20 +27,40 @@ namespace Applications.DicomDirectoryProcessor.Execution
         {
             _cliOptions = cliOptions;
 
-            if (!Directory.Exists(globals.FileSystemOptions.FileSystemRoot))
-                throw new ArgumentException("Cannot find the FileSystemRoot specified in the given MicroservicesOptions (" + globals.FileSystemOptions.FileSystemRoot + ")");
+	    if (!cliOptions.DirectoryFormat.ToLower().Equals("list"))
+	    {
+		Logger.Info("This indicates that the list mode is not being recognised");
+                if (!Directory.Exists(globals.FileSystemOptions.FileSystemRoot))
+                    throw new ArgumentException("Cannot find the FileSystemRoot specified in the given MicroservicesOptions (" + globals.FileSystemOptions.FileSystemRoot + ")");
 
-            if (!cliOptions.ToProcessDir.Exists)
-                throw new ArgumentException("Could not find directory " + cliOptions.ToProcessDir.FullName);
+                if (!cliOptions.ToProcessDir.Exists)
+                    throw new ArgumentException("Could not find directory " + cliOptions.ToProcessDir.FullName);
 
-            if (!cliOptions.ToProcessDir.FullName.StartsWith(globals.FileSystemOptions.FileSystemRoot, true, CultureInfo.CurrentCulture))
-                throw new ArgumentException("Directory parameter (" + cliOptions.ToProcessDir.FullName + ") must be below the FileSystemRoot (" + globals.FileSystemOptions.FileSystemRoot + ")");
+                if (!cliOptions.ToProcessDir.FullName.StartsWith(globals.FileSystemOptions.FileSystemRoot, true, CultureInfo.CurrentCulture))
+                    throw new ArgumentException("Directory parameter (" + cliOptions.ToProcessDir.FullName + ") must be below the FileSystemRoot (" + globals.FileSystemOptions.FileSystemRoot + ")");
+            }
+	    else
+            {
+		Logger.Info("This indicates that the list mode is being recognised");
+		if (!File.Exists(cliOptions.ToProcessDir.FullName))
+		    throw new ArgumentException("Could not find accession directory list file (" + cliOptions.ToProcessDir.FullName + ")");
+		
+		if (!Path.GetExtension(cliOptions.ToProcessDir.FullName).Equals(".csv"))
+		    throw new ArgumentException("When in 'list' mode, path to accession directory file of format .csv expected (" + cliOptions.ToProcessDir.FullName + ")");
+	    }
 
             if (cliOptions.DirectoryFormat.ToLower().Equals("pacs"))
             {
                 Logger.Info("Creating PACS directory finder");
 
                 _ddf = new PacsDirectoryFinder(globals.FileSystemOptions.FileSystemRoot,
+                    globals.FileSystemOptions.DicomSearchPattern, RabbitMqAdapter.SetupProducer(globals.ProcessDirectoryOptions.AccessionDirectoryProducerOptions));
+            }
+            else if (cliOptions.DirectoryFormat.ToLower().Equals("list"))
+            {
+                Logger.Info("Creating accession directory lister");
+
+                _ddf = new AccessionDirectoryLister(globals.FileSystemOptions.FileSystemRoot,
                     globals.FileSystemOptions.DicomSearchPattern, RabbitMqAdapter.SetupProducer(globals.ProcessDirectoryOptions.AccessionDirectoryProducerOptions));
             }
             else if (cliOptions.DirectoryFormat.ToLower().Equals("default"))
---
+++ src/applications/Applications.DicomDirectoryProcessor/Execution/DirectoryFinders/AccessionDirectoryLister.cs
@@ -0,0 +1,63 @@
+
+using Smi.Common.Messaging;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Abstractions;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Applications.DicomDirectoryProcessor.Execution.DirectoryFinders
+{
+    public class AccessionDirectoryLister : DicomDirectoryFinder
+    {
+        // Regex that matches when we are at the yyyy\mm\dd\xxxxx directory level
+        private readonly Regex _accDirectoryRegex = new Regex(@"(20\d{2}[\\\/]\d{2}[\\\/]\d{2})[\\\/]\w+[^.]*?$");
+
+        public AccessionDirectoryLister(string fileSystemRoot, IFileSystem fileSystem, string dicomSearchPattern, IProducerModel directoriesProducerModel)
+        : base(fileSystemRoot, fileSystem, dicomSearchPattern, directoriesProducerModel) { }
+
+        public AccessionDirectoryLister(string fileSystemRoot, string dicomSearchPattern, IProducerModel directoriesProducerModel)
+            : this(fileSystemRoot, new FileSystem(), dicomSearchPattern, directoriesProducerModel) { }
+
+
+        public override void SearchForDicomDirectories(string accessionsList)
+        {
+            Logger.Info("Starting accession directory path listing from: " + accessionsList);
+            IsProcessing = true;
+            TotalSent = 0;
+
+	    using(var reader = new StreamReader(@accessionsList))
+	    {
+		while (!reader.EndOfStream)
+		{
+                    string accessionDirectory = reader.ReadLine().Replace(",", "");
+
+                    if (_accDirectoryRegex.IsMatch(accessionDirectory))
+                    {
+                        if (!FileSystem.Directory.Exists(accessionDirectory))
+                        {
+                            Logger.Warn("Can not find " + accessionDirectory + ", continuing");
+                            continue;
+                        }
+		        else
+                        {
+                            Logger.Debug("Sending message (" + accessionDirectory + ")");
+                            FoundNewDicomDirectory(accessionDirectory.Remove(0, FileSystemRoot.Length));
+                        }
+                    }
+                    else
+                    {
+                        Logger.Warn("This path does not point to an accession directory: (" + accessionDirectory + "), continuing");
+		        continue;
+	            }
+		}
+            }
+
+            IsProcessing = false;
+
+            Logger.Info("Reading from list finished");
+            Logger.Info("Total messages sent: " + TotalSent);
+        }
+    }
+}
+
