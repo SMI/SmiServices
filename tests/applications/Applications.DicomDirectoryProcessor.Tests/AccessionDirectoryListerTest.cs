a6bb0e41d27e27b07f5771f2aac6a31715636293
Bianca <b.prodan95@gmail.com>
2020-02-17 11:38:38 +0000

    BP: Add unit test for AccessionDirectoryLister with abstract mock file

--- tests/applications/Applications.DicomDirectoryProcessor.Tests/AccessionDirectoryListerTest.cs
+++ tests/applications/Applications.DicomDirectoryProcessor.Tests/AccessionDirectoryListerTest.cs
@@ -1,6 +1,15 @@
 ﻿
+using Applications.DicomDirectoryProcessor.Execution.DirectoryFinders;
+using Moq;
 using NUnit.Framework;
+using Smi.Common.Messages;
+using Smi.Common.Messaging;
 using Smi.Common.Tests;
+using System.Collections.Generic;
+using System;
+using System.Text;
+using System.IO;
+using System.IO.Abstractions.TestingHelpers;
 
 
 namespace Applications.DicomDirectoryProcessor.Tests
@@ -17,8 +26,46 @@ namespace Applications.DicomDirectoryProcessor.Tests
             TestLogger.Setup();
         }
         
-        // TODO(rkm 2020-02-12) Things to test
-        // - Valid CSV file
-        // - CSVs with various invalid data / lines
+	private String GetListContent()
+	{
+            StringBuilder accessionList = new StringBuilder();
+
+	    accessionList.AppendLine("/PACS/2018/01/01/AAA,");
+	    accessionList.AppendLine("/PACS/2018/01/01/AAA/,");
+	    accessionList.AppendLine("/PACS/2018/01/01/,");
+	    accessionList.AppendLine("/PACS/2018/01/01/test.dcm,");
+	    accessionList.AppendLine("                 ");
+	    accessionList.AppendLine("NULL");
+	    accessionList.AppendLine(",,,,");
+
+	    return accessionList.ToString(); 
+	}
+
+	[Test]
+	public void TestAccessionDirectoryLister()
+        {
+            // Mock file system referenced in accession list
+            string rootDir = Path.GetFullPath("/PACS");
+            MockFileSystem mockFilesystem = new MockFileSystem();
+	    
+	    // Mock input file 
+	    string testFile = Path.GetFullPath(Path.Combine(rootDir, "2018/01/01/AAA/testDicom.dcm"));
+	    var mockInputFile = new MockFileData(GetListContent());
+	    mockFilesystem.AddFile(testFile, mockInputFile);
+
+	    // Mock producer
+	    var totalSent = 0;
+
+            Mock<IProducerModel> mockProducerModel = new Mock<IProducerModel>();
+            mockProducerModel
+                .Setup(x => x.SendMessage(It.IsAny<IMessage>(),
+                                            null,
+                                            ""))
+                .Callback(() => ++totalSent);
+
+            AccessionDirectoryLister accessionLister = new AccessionDirectoryLister(rootDir, mockFilesystem, "*.dcm", mockProducerModel.Object);
+
+            accessionLister.SearchForDicomDirectories(testFile);
+        }
     }
 }
