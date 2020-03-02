1d85f39520d80c70a3ac87a577eceacd24eea2a5
Thomas Nind <TZNind@dundee.ac.uk>
2020-03-02 08:54:18 +0000

    Added some helper method for FailurePart

---
+++ tests/microservices/Microservices.IsIdentifiable.Tests/StoreReportTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microservices.IsIdentifiable.Failure;
+using Microservices.IsIdentifiable.Reporting.Destinations;
+using NUnit.Framework;
+
+namespace Microservices.IsIdentifiable.Tests
+{
+    class StoreReportTests
+    {
+
+        [Test]
+        public void Test_Includes()
+        {
+            string origin = "this word fff is the problem";
+            
+            var part = new FailurePart("fff", FailureClassification.Organization, origin.IndexOf("fff"));
+            
+            Assert.IsFalse(part.Includes(0));
+            Assert.IsFalse(part.Includes(9));
+            Assert.IsTrue(part.Includes(10));
+            Assert.IsTrue(part.Includes(11));
+            Assert.IsTrue(part.Includes(12));
+            Assert.IsFalse(part.Includes(13));
+        }
+
+        [Test]
+        public void Test_IncludesSingleChar()
+        {
+            string origin = "this word f is the problem";
+            
+            var part = new FailurePart("f", FailureClassification.Organization, origin.IndexOf("f"));
+            
+            Assert.IsFalse(part.Includes(0));
+            Assert.IsFalse(part.Includes(9));
+            Assert.IsTrue(part.Includes(10));
+            Assert.IsFalse(part.Includes(11));
+            Assert.IsFalse(part.Includes(12));
+            Assert.IsFalse(part.Includes(13));
+        }
+    }
+}
