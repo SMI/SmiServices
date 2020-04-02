fdd83b1913135970464d355f2e641dd868e8ded2
James A Sutherland <j@sutherland.pw>
2020-04-02 16:16:47 +0100

    Clean up empty test case wrapper

--- src/common/com.smi.microservices.common/src/test/java/org/smi/common/test/options/GlobalOptionsTest.java
+++ src/common/com.smi.microservices.common/src/test/java/org/smi/common/test/options/GlobalOptionsTest.java
@@ -5,24 +5,9 @@ import org.smi.common.options.GlobalOptions;
 import junit.framework.TestCase;
 
 public class GlobalOptionsTest extends TestCase {
-	
-	private GlobalOptions _options;
-	
-	protected void setUp() throws Exception {
-		
-		super.setUp();
-		_options = GlobalOptions.Load(true);
-	}
 
-	protected void tearDown() throws Exception {
-		
-		_options = null;
-		super.tearDown();
-	}
-	
-	//TODO Add proper tests here	
-	
-	public void testEmpty() {
-		
-	}
+    protected void setUp() throws Exception {
+        super.setUp();
+        GlobalOptions.Load(true);
+    }
 }
