4d253b9e27970a8ce9d9a2bdd83747103b3eb198
tznind <t.z.nind@dundee.ac.uk>
2020-07-27 11:07:49 +0100

    Removed always property from NationalPACSAccessionNumber

--- src/common/Smi.Common/Messages/DicomFileMessage.cs
+++ src/common/Smi.Common/Messages/DicomFileMessage.cs
@@ -16,7 +16,6 @@ namespace Smi.Common.Messages
         /// <summary>
         /// NationalPACSAccessionNumber obtained from the end of the directory path.
         /// </summary>
-        [JsonProperty(Required = Required.Always)]
         public string NationalPACSAccessionNumber { get; set; }
 
         /// <summary>
