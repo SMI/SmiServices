b1e9d9fd80c583361214d9af025169eb287a1217
Ruairidh MacLeod <5160559+rkm@users.noreply.github.com>
2020-08-25 19:55:59 +0100

    Add FileCopier test proj

---
+++ tests/microservices/Microservices.FileCopier.Tests/Execution/FileCopierTest.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+
+namespace Microservices.FileCopier.Tests.Execution
+{
+    public class FileCopierTest
+    {
+        #region Fixture Methods 
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp() { }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown() { }
+
+        #endregion
+
+        #region Test Methods
+
+        [SetUp]
+        public void SetUp() { }
+
+        [TearDown]
+        public void TearDown() { }
+
+        #endregion
+
+        #region Tests
+
+        [Test]
+        public void ExampleTest()
+        {
+            Assert.Fail();
+        }
+
+        #endregion
+    }
+}
