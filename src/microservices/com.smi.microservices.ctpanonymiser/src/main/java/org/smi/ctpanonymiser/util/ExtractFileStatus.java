1d20cf3b4b7aee968ad2c730ff96528663d4d691
Ruairidh MacLeod <ruairidh@rkm.dev>
2020-08-26 22:32:30 +0100

    Make Java ExtractFileStatus match c#
    New fields are unused in java-land

--- src/microservices/com.smi.microservices.ctpanonymiser/src/main/java/org/smi/ctpanonymiser/util/ExtractFileStatus.java
+++ src/microservices/com.smi.microservices.ctpanonymiser/src/main/java/org/smi/ctpanonymiser/util/ExtractFileStatus.java
@@ -18,5 +18,15 @@ public enum ExtractFileStatus {
 	/**
 	 * The file could not be anonymised and will not be retired
 	 */
-	ErrorWontRetry;
+	ErrorWontRetry,
+
+	/**
+	 * The source file could not be found under the given filesystem root
+	 */
+	FileMissing,
+
+	/**
+	 * The source file was successfully copied to the destination
+	 */
+	Copied,
 }
