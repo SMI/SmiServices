7914d167713d9717192a946dc543c404a9576483
tznind <t.z.nind@dundee.ac.uk>
2020-09-09 10:54:08 +0100

    Fixed producer not being IOptions

--- src/common/Smi.Common/Options/GlobalOptions.cs
+++ src/common/Smi.Common/Options/GlobalOptions.cs
@@ -142,7 +142,7 @@ namespace Smi.Common.Options
     }
 
     [UsedImplicitly]
-    public class IdentifierMapperOptions : ConsumerOptions, IMappingTableOptions
+    public class IdentifierMapperOptions : ConsumerOptions
     {
         public ProducerOptions AnonImagesProducerOptions { get; set; }
         public string MappingConnectionString { get; set; }
