58c5e4751bec2fdf9f37b0dda9fbe2bd787c38ae
Ruairidh MacLeod <ruairidh@rkm.dev>
2020-09-23 10:57:59 +0100

    Move extension choice into DefaultProjectPathResolver

--- tests/microservices/Microservices.CohortExtractor.Tests/DefaultProjectPathResolverTest.cs
+++ tests/microservices/Microservices.CohortExtractor.Tests/DefaultProjectPathResolverTest.cs
@@ -1,14 +1,47 @@
-﻿
-using Microservices.CohortExtractor.Execution.ProjectPathResolvers;
+﻿using Microservices.CohortExtractor.Execution.ProjectPathResolvers;
 using Microservices.CohortExtractor.Execution.RequestFulfillers;
 using NUnit.Framework;
+using Smi.Common.Messages.Extraction;
+using Smi.Common.Tests;
 using System.IO;
+
 
 namespace Microservices.CohortExtractor.Tests
 {
-    [TestFixture]
     public class DefaultProjectPathResolverTest
     {
+        #region Fixture Methods
+
+        private ExtractionRequestMessage _requestMessage;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            TestLogger.Setup();
+
+            _requestMessage = new ExtractionRequestMessage
+            {
+                IsIdentifiableExtraction = false,
+            };
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown() { }
+
+        #endregion
+
+        #region Test Methods
+
+        [SetUp]
+        public void SetUp() { }
+
+        [TearDown]
+        public void TearDown() { }
+
+        #endregion
+
+        #region Tests
+
         [TestCase("study", "series")]
         [TestCase("study", null)]
         [TestCase(null, "series")]
@@ -28,7 +61,7 @@ namespace Microservices.CohortExtractor.Tests
                     study ?? "unknown",
                     series ?? "unknown",
                     "foo-an.dcm"),
-                new DefaultProjectPathResolver().GetOutputPath(result, null));
+                new DefaultProjectPathResolver().GetOutputPath(result, _requestMessage));
         }
 
         [TestCase("file-an.dcm", "file.dcm")]
@@ -50,7 +83,7 @@ namespace Microservices.CohortExtractor.Tests
                     "study",
                     "series",
                     expectedOutput),
-                new DefaultProjectPathResolver().GetOutputPath(result, null));
+                new DefaultProjectPathResolver().GetOutputPath(result, _requestMessage));
         }
 
         [Test]
@@ -69,7 +102,33 @@ namespace Microservices.CohortExtractor.Tests
                     "study",
                     "unknown",
                     "file-an.dcm"),
-                new DefaultProjectPathResolver().GetOutputPath(result, null));
+                new DefaultProjectPathResolver().GetOutputPath(result, _requestMessage));
         }
+
+        [Test]
+        public void Test_DefaultProjectPathResolver_IdentExtraction()
+        {
+            var requestMessage = new ExtractionRequestMessage
+            {
+                IsIdentifiableExtraction = true,
+            };
+
+            var result = new QueryToExecuteResult(
+                Path.Combine("foo", "file"),
+                "study",
+                null,
+                "sop",
+                false,
+                null);
+
+            Assert.AreEqual(
+                Path.Combine(
+                    "study",
+                    "unknown",
+                    "file.dcm"),
+                new DefaultProjectPathResolver().GetOutputPath(result, requestMessage));
+        }
+
+        #endregion
     }
 }
