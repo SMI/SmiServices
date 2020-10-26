8069e408efb24e2895618712182f8046add0aef1
tznind <t.z.nind@dundee.ac.uk>
2020-10-26 14:35:00 +0000

    Added ConsensusRule class that intersects 2 other rules e.g. NLP classifiers

---
+++ tests/microservices/Microservices.IsIdentifiable.Tests/ConsensusRuleTests.cs
@@ -0,0 +1,91 @@
+﻿using Microservices.IsIdentifiable.Failures;
+using Microservices.IsIdentifiable.Rules;
+using Moq;
+using NPOI.OpenXmlFormats.Wordprocessing;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Microservices.IsIdentifiable.Tests
+{
+    class ConsensusRuleTests
+    {
+        [Test]
+        public void NoConsensus_OnlyOneHasProblem()
+        {
+            var rule = new ConsensusRule()
+            {
+                Rules = new ICustomRule[]
+                {
+                    new TestRule(RuleAction.Report,new FailurePart("bb",FailureClassification.Person)),
+                    new TestRule(RuleAction.Ignore),
+                }
+            };
+
+            var result = rule.Apply("ff","vv",out IEnumerable<FailurePart> badParts);
+
+            Assert.AreEqual(RuleAction.None,result);
+            Assert.IsEmpty(badParts);
+        }
+
+        [TestCase(-1)]
+        [TestCase(10)]
+        public void Consensus_Exact(int offset)
+        {
+            var rule = new ConsensusRule()
+            {
+                Rules = new ICustomRule[]
+                {
+                    new TestRule(RuleAction.Report,new FailurePart("bb",FailureClassification.Person,offset)),
+                    new TestRule(RuleAction.Report,new FailurePart("bb",FailureClassification.Person,offset)),
+                }
+            };
+
+            var result = rule.Apply("ff","vv",out IEnumerable<FailurePart> badParts);
+
+            Assert.AreEqual(RuleAction.Report,result);
+            Assert.AreEqual(offset,badParts.Single().Offset);
+        }
+
+        [Test]
+        public void Consensus_SingleOverlap()
+        {
+            var rule = new ConsensusRule()
+            {
+                Rules = new ICustomRule[]
+                {
+                    // Word is length 2 and begins at offset 10
+                    new TestRule(RuleAction.Report,new FailurePart("ab",FailureClassification.Person,10)),
+                    new TestRule(RuleAction.Report,new FailurePart("bc",FailureClassification.Person,11)),
+                }
+            };
+
+            var result = rule.Apply("ff","abc is so cool",out IEnumerable<FailurePart> badParts);
+
+            Assert.AreEqual(RuleAction.Report,result);
+            Assert.AreEqual(10,badParts.Single().Offset);
+            Assert.AreEqual("ab",badParts.Single().Word);
+        }
+
+        class TestRule : ICustomRule
+        {
+            RuleAction _rule;
+            FailurePart[] _parts;
+
+            public TestRule(RuleAction rule, params FailurePart[] parts)
+            {
+                _parts = parts;
+                _rule = rule;
+            }
+
+            public RuleAction Apply(string fieldName, string fieldValue, out IEnumerable<FailurePart> badParts)
+            {
+                badParts = _parts;
+
+                return _rule;
+            }
+        }
+    }
+}
