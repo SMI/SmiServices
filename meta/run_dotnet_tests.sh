e8cd1f54c65e19835daf04a150c095a48afbec9c
Ruairidh MacLeod <ruairidh@rkm.dev>
2020-12-04 13:55:58 +0000

    split test runs

---
+++ meta/run_dotnet_tests.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# NOTE(rkm 2020-12-04) This script will exit immediately if and command fails. This is
+# different to the old Travis builds which would still run each line and only report
+# failures at the end
+
+set -euxo pipefail
+
+dotnet build -c Release --verbosity quiet
+
+for i in `find . -type d -name netcoreapp3.1`; do
+    if [ ! -e $i/default.yaml ]; then
+        ln data/microserviceConfigs/default.yaml $i/ ;
+    fi
+done
+
+find ./tests -iname "*.csproj" -print0 \
+    | xargs -0 -L 1 \
+    dotnet test \
+    -p:Platform=x64 \
+    --configuration Release \
+    --verbosity normal \
+    --no-build \
+    --settings data/nunit.runsettings
---
+++ meta/run_java_tests.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# NOTE(rkm 2020-12-04) This script will exit immediately if and command fails. This is
+# different to the old Travis builds which would still run each line and only report
+# failures at the end
+
+set -euxo pipefail
+
+# TODO(rkm 2020-12-04) Is this still needed?
+if [ -f lib/java/Util/source/java/org/rsna/util/ChunkedInputStream.java ]; then
+    echo "Running iconv on ChunkedInputStream.java"
+    iconv -f iso-8859-1 -t utf-8 lib/java/Util/source/java/org/rsna/util/ChunkedInputStream.java > tmpiconv
+    mv tmpiconv lib/java/Util/source/java/org/rsna/util/ChunkedInputStream.java
+fi
+
+mvn -ntp -q -f src/common/com.smi.microservices.parent/pom.xml test
+mvn -ntp -q -f src/microservices/uk.ac.dundee.hic.nerd/ test
