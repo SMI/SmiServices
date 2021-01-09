3cbf943ffbc85eacaddc18d585a79bc1c341c242
Ruairidh MacLeod <ruairidh@rkm.dev>
2021-01-09 14:10:04 +0000

    remove -x

--- meta/run_dotnet_tests.sh
+++ meta/run_dotnet_tests.sh
@@ -4,7 +4,7 @@
 # different to the old Travis builds which would still run each line and only report
 # failures at the end
 
-set -uxo pipefail
+set -uo pipefail
 
 if [ ! -f data/tessdata/eng.traineddata ]
 then
