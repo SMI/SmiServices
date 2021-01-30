a48f5982ad47f7e996313850b60a3616c5939455
Ruairidh MacLeod <ruairidh@rkm.dev>
2021-01-30 15:18:03 +0000

    automatically add changelog to releases

---
+++ .azure-pipelines/scripts/print_release_changelog.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import argparse
+from typing import Optional
+from typing import Sequence
+
+
+def main(argv: Optional[Sequence[str]] = None) -> int:
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("tag", help="The git tag for the release")
+    args = parser.parse_args(argv)
+    tag = args.tag[1:]
+
+    release_lines = []
+    with open("CHANGELOG.md") as f:
+        reading = False
+        while True:
+            line = f.readline().strip()
+            if not reading:
+                if tag in line:
+                    reading = True
+                    release_lines.append(f"## {tag}")
+                continue
+            if reading and line.startswith("## ["):
+                break
+            release_lines.append(line)
+
+    with open("release_changelog.md", "w") as f:
+        f.write("\n".join(release_lines))
+
+
+if __name__ == "__main__":
+    exit(main())
