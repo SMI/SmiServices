63fcbe68518712bd9d2a72dc3892e02029cee38c
Ruairidh MacLeod <ruairidh@rkm.dev>
2021-03-02 17:23:41 +0000

    add initial update-changelog script

---
+++ utils/update-changelog.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+"""
+TODO:
+Update links at bottom of changelog
+Run formatter
+Delete news files
+"""
+import argparse
+import collections
+import datetime
+import fileinput
+import json
+import sys
+import urllib.request
+from pathlib import Path
+from typing import Dict
+from typing import Optional
+from typing import Sequence
+
+
+_NEWS_DIR = Path("./news/")
+_MARKER = "<!--next-->"
+
+_ORG = "SMI"
+_REPO = "SmiServices"
+
+
+def _get_pr_author(pr_ref: int) -> str:
+
+    url = (
+        f"https://api.github.com/repos/"
+        f"{_ORG}/{_REPO}/"
+        f"pulls/{pr_ref}"
+    )
+    resp = urllib.request.urlopen(url)
+    data = json.loads(resp.read().decode())
+    return data["user"]["login"]
+
+
+def _print_fragments(version: str, fragments: Dict[str, Dict[int, str]]) -> None:
+
+    today = datetime.datetime.today().strftime("%Y-%m-%d")
+    print(f"## [{version[1:]}] {today}")
+
+    def _print_type_fragment(frag_type: str, fragments: Dict[str, Dict[int, str]]):
+        print(f"\n## {frag_type.capitalize()}\n")
+        for pr_ref in sorted(fragments[frag_type]):
+            first, *rest = fragments[frag_type][pr_ref].splitlines()
+            list_items = ""
+            for li in rest:
+                list_items += f"\n    {li}"
+            author = _get_pr_author(pr_ref)
+            line = (
+                "-   "
+                f"[#{pr_ref}](https://github.com/SMI/SmiServices/pull/{pr_ref})"
+                f" by {author}. "
+                f"{first}"
+                f"{list_items}"
+            )
+            print(line)
+
+    # New features first
+    _print_type_fragment("feature", fragments)
+
+    # Then the rest
+    for frag_type in sorted(fragments):
+        if frag_type == "feature":
+            continue
+        _print_type_fragment(frag_type, fragments)
+
+
+def main(argv: Optional[Sequence[str]] = None) -> int:
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "release_tag",
+        help="The tag for the next release",
+    )
+    args = parser.parse_args(argv)
+    tag = args.release_tag
+
+    fragments = collections.defaultdict(dict)
+    for fragment_file in _NEWS_DIR.glob("*-*.md"):
+        with open(fragment_file) as f:
+            contents = f.read()
+        pr_ref, _, frag_type = fragment_file.stem.partition("-")
+        fragments[frag_type][pr_ref] = contents
+
+    with fileinput.FileInput("CHANGELOG.md", inplace=True) as f:
+        for line in f:
+            if _MARKER in line:
+                print(line)
+                _print_fragments(tag, fragments)
+                continue
+            print(line, end="")
+
+    return 0
+
+if __name__ == "__main__":
+    exit(main())
+
