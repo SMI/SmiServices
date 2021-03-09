2ac22a7d3d3c5f9b3bc7c2ecb2dfe98d4d537a78
Ruairidh MacLeod <ruairidh@rkm.dev>
2021-03-09 09:35:33 +0000

    add python tests to CI

--- .azure-pipelines/scripts/run-python-tests.bash
+++ .azure-pipelines/scripts/run-python-tests.bash
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
-# Requires an environment with pytest installed,
-# plus all the requirements of the SmiServices package.
 
 set -euo pipefail
 
-python -m pytest src/common/Smi_Common_Python/SmiServices/*.py
+./venv/bin/python -m pytest src/common/Smi_Common_Python/SmiServices/*.py
