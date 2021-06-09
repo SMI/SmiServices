04e87e114b1ec009a112ab7332be1d8a1e0663da
Ruairidh MacLeod <ruairidh@rkm.dev>
2021-06-09 17:01:16 +0000

    use correct vars from external tests

--- .azure-pipelines/scripts/upload-coverage.bash
+++ .azure-pipelines/scripts/upload-coverage.bash
@@ -1,28 +1,30 @@
 #!/usr/bin/env bash
 
-set -euxo pipefail
+set -exo pipefail
 
-dotnet tool update --global coveralls.net
+#dotnet tool update --global coveralls.net
 
-if [ ! -z "$(System.PullRequest.PullRequestNumber)" ]
+if [ ! -z "$SYSTEM_PULLREQUEST_PULLREQUESTNUMBER" ]
 then
-    commit_id_arg="--commitId $(System.PullRequest.SourceCommitId)"
-    commit_branch_arg="--commitBranch $(System.PullRequest.SourceBranch)"
-    pull_request_arg="--pullRequest $(System.PullRequest.PullRequestNumber)"
+    commit_branch_arg="--commitBranch $SYSTEM_PULLREQUEST_SOURCEBRANCH"
+    commit_id_arg="--commitId $SYSTEM_PULLREQUEST_SOURCECOMMITID"
+    pull_request_arg="--pullRequest $SYSTEM_PULLREQUEST_PULLREQUESTNUMBER"
 else
-    commit_id_arg="--commitId $(Build.SourceVersion)"
-    commit_branch_arg="--commitBranch $(Build.SourceVersion)"
+    commit_branch_arg="--commitBranch $BUILD_SOURCEBRANCHNAME"
+    commit_id_arg="--commitId $BUILD_SOURCEVERSION"
     pull_request_arg=""
 fi
 
-csmacnz.Coveralls \
+set -u
+
+echo csmacnz.Coveralls \
     --opencover \
     -i coverage/coverage.opencover.xml \
     --useRelativePaths \
+    --commitAuthor "$BUILD_SOURCEVERSIONAUTHOR" \
+    --commitEmail "$BUILD_REQUESTEDFOREMAIL" \
+    --commitMessage "$BUILD_SOURCEVERSIONMESSAGE" \
+    --jobId "$BUILD_BUILDID"
     $commit_id_arg \
     $commit_branch_arg \
-    $pull_request_arg \
-    --commitAuthor "$(git show -s --format='%an')" \
-    --commitEmail "$(git show -s --format='%ae')" \
-    --commitMessage "$(Build.SourceVersionMessage)" \
-    --jobId "$(Build.BuildId)"
+    $pull_request_arg
