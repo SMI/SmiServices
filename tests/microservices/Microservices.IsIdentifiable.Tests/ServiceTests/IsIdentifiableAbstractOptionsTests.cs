2d388c4a5dee31a296a63b0ee5d69c3a30e98b3b
Thomas Nind <TZNind@dundee.ac.uk>
2021-06-14 10:48:37 +0100

    Added test to verify we are not overriding when we shouldn't be

--- tests/microservices/Microservices.IsIdentifiable.Tests/ServiceTests/IsIdentifiableAbstractOptionsTests.cs
+++ tests/microservices/Microservices.IsIdentifiable.Tests/ServiceTests/IsIdentifiableAbstractOptionsTests.cs
@@ -52,6 +52,58 @@ namespace Microservices.IsIdentifiable.Tests.ServiceTests
 
         }
 
+        [Test]
+        public void FillMissingWithValuesUsing_NoOverride()
+        {
+            int propsCounted = 0;
+
+            foreach (var gProp in typeof(IsIdentifiableOptions).GetProperties())
+            {
+                var cliProp = typeof(IsIdentifiableDicomFileOptions).GetProperty(gProp.Name);
+
+                if (cliProp == null)
+                {
+                    continue;
+                }
+
+                var opts = new IsIdentifiableDicomFileOptions();
+                var globalOpts = new IsIdentifiableOptions();
+
+                var testVal1 = GetTestValue(cliProp);
+                var testVal2 = GetTestValue2(cliProp);
+
+                if(testVal1 is bool)
+                {
+                    // boolean cli false is the default so missing and false are the same
+                    // so instead lets make sure that false in yaml config doesn't override 
+                    // true in cli
+                    testVal1 = false;
+                    testVal2 = true;
+                }
+
+                // yaml says one value
+                gProp.SetValue(globalOpts, testVal1);
+                // cli says a different value
+                cliProp.SetValue(opts, testVal2);
+                
+                // we should not have the yaml file entry
+                Assert.AreNotEqual(testVal1, cliProp.GetValue(opts));
+                
+                // we ask to fill in missing values using the yaml entries
+                opts.FillMissingWithValuesUsing(globalOpts);
+
+                // but we had an entry on CLI already so that should take precedence
+                Assert.AreNotEqual(testVal1, cliProp.GetValue(opts));
+                Assert.AreEqual(testVal2, cliProp.GetValue(opts));
+
+                propsCounted++;
+            }
+
+            // we did test some properties right!
+            Assert.Greater(propsCounted, 0);
+
+        }
+
         private object GetTestValue(System.Reflection.PropertyInfo gProp)
         {
             if(gProp.PropertyType == typeof(int))
@@ -66,6 +118,30 @@ namespace Microservices.IsIdentifiable.Tests.ServiceTests
             if (gProp.PropertyType == typeof(bool))
             {
                 return true;
+            }
+
+            if (gProp.PropertyType == typeof(DatabaseType))
+            {
+                return DatabaseType.MySql;
+            }
+
+            throw new ArgumentException($"Not sure what value to use in test for PropertyType {gProp.PropertyType}.  This is an error in the test harness coverage not the underlying code.");
+
+        }
+        private object GetTestValue2(System.Reflection.PropertyInfo gProp)
+        {
+            if (gProp.PropertyType == typeof(int))
+            {
+                return 66456;
+            }
+
+            if (gProp.PropertyType == typeof(string))
+            {
+                return "rylyly?";
+            }
+            if (gProp.PropertyType == typeof(bool))
+            {
+                return false;
             }
 
             if (gProp.PropertyType == typeof(DatabaseType))
