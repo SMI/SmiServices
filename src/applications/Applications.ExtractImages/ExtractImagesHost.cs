2d454864b8ae7d5836b653f66379bd1767809fdb
Ruairidh MacLeod <ruairidh@rkm.dev>
2021-12-13 12:20:50 +0000

    Revert variable rename to clear up PR diff

--- src/applications/Applications.ExtractImages/ExtractImagesHost.cs
+++ src/applications/Applications.ExtractImages/ExtractImagesHost.cs
@@ -44,9 +44,9 @@ namespace Applications.ExtractImages
 
             _fileSystem = fileSystem ?? new FileSystem();
 
-            string extractionRoot = Globals.FileSystemOptions.ExtractRoot;
-            if (!_fileSystem.Directory.Exists(extractionRoot))
-                throw new DirectoryNotFoundException($"Could not find the extraction root '{extractionRoot}'");
+            string extractRoot = Globals.FileSystemOptions.ExtractRoot;
+            if (!_fileSystem.Directory.Exists(extractRoot))
+                throw new DirectoryNotFoundException($"Could not find the extraction root '{extractRoot}'");
 
             _csvFilePath = cliOptions.CohortCsvFile;
             if (string.IsNullOrWhiteSpace(_csvFilePath))
@@ -58,7 +58,7 @@ namespace Applications.ExtractImages
             //                      to a helper class to support having multiple configurations (and probably prevent some bugs)
             string extractionName = _fileSystem.Path.GetFileNameWithoutExtension(_csvFilePath);
             string extractionDir = _fileSystem.Path.Join(cliOptions.ProjectId, "extractions", extractionName);
-            _absoluteExtractionDir = _fileSystem.Path.Join(extractionRoot, extractionDir);
+            _absoluteExtractionDir = _fileSystem.Path.Join(extractRoot, extractionDir);
 
             if (_fileSystem.Directory.Exists(_absoluteExtractionDir))
                 throw new DirectoryNotFoundException($"Extraction directory already exists '{_absoluteExtractionDir}'");
@@ -74,7 +74,7 @@ namespace Applications.ExtractImages
                     extractionRequestProducer,
                     extractionRequestInfoProducer,
                     _fileSystem,
-                    extractionRoot,
+                    extractRoot,
                     extractionDir,
                     new DateTimeProvider(),
                     new RealConsoleInput()
