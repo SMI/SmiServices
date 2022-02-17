1c650f339d32a216467b8d6c6b2df280751146a4
Ruairidh MacLeod <ruairidh@rkm.dev>
2022-02-17 15:08:21 +0000

    bump versions

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -7,6 +7,6 @@ using System.Reflection;
 [assembly: AssemblyCulture("")]
 
 // These should be overwritten by release builds
-[assembly: AssemblyVersion("4.0.0")]
-[assembly: AssemblyFileVersion("4.0.0")]
-[assembly: AssemblyInformationalVersion("4.0.0")] // This one can have the extra build info after it
+[assembly: AssemblyVersion("5.0.0")]
+[assembly: AssemblyFileVersion("5.0.0")]
+[assembly: AssemblyInformationalVersion("5.0.0")] // This one can have the extra build info after it
