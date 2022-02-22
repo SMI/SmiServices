8f068165ab8774773cd1c4a8534603ecf89263fa
Ruairidh MacLeod <ruairidh@rkm.dev>
2022-02-22 17:12:03 +0000

    Add FailureData helper class

---
+++ src/microservices/Microservices.CohortPackager/Execution/JobProcessing/Reporting/FailureData.cs
@@ -0,0 +1,51 @@
+﻿using Equ;
+using Microservices.IsIdentifiable.Failures;
+using Microservices.IsIdentifiable.Reporting;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace Microservices.CohortPackager.Execution.JobProcessing.Reporting
+{
+    /// <summary>
+    /// Helper class to describe the information from a <see cref="Failure"/> without the associated Resource or
+    /// ResourcePrimaryKey, allowing easier sorting and filtering in collections.
+    /// </summary>
+    public class FailureData : MemberwiseEquatable<FailureData>, IComparable<FailureData>
+    {
+        /// <summary>
+        /// See <see cref="Failure.Parts"/>
+        /// </summary>
+        public ReadOnlyCollection<FailurePart> Parts { get; private set; }
+
+        /// <summary>
+        /// See <see cref="Failure.ProblemField"/>
+        /// </summary>
+        public string ProblemField { get; private set; }
+
+        /// <summary>
+        /// See <see cref="Failure.ProblemValue"/>
+        /// </summary>
+        public string ProblemValue { get; private set; }
+
+        public FailureData(
+            IEnumerable<FailurePart> parts,
+            string problemField,
+            string problemValue
+        )
+        {
+            if (parts is null) throw new ArgumentNullException(nameof(parts));
+            var partsList = parts.ToList();
+            Parts = (partsList.Count == 0) ? throw new ArgumentException("No failure parts provided") : new ReadOnlyCollection<FailurePart>(partsList);
+
+            ProblemField = (string.IsNullOrWhiteSpace(problemField)) ? throw new ArgumentException("Cannot be null", nameof(problemField)) : problemField;
+            ProblemValue = (string.IsNullOrWhiteSpace(problemValue)) ? throw new ArgumentException("Cannot be null", nameof(problemValue)) : problemValue;
+        }
+
+        public static FailureData FromFailure(Failure f) => new(f.Parts, f.ProblemField, f.ProblemValue);
+
+        // ^IComparable<FailureData>
+        public int CompareTo(FailureData other) => Parts.Count.CompareTo(other.Parts.Count);
+    }
+}
---
+++ tests/microservices/Microservices.CohortPackager.Tests/Execution/JobProcessing/Reporting/FailureDataTests.cs
@@ -0,0 +1,153 @@
+﻿using Microservices.CohortPackager.Execution.JobProcessing.Reporting;
+using Microservices.IsIdentifiable.Failures;
+using Microservices.IsIdentifiable.Reporting;
+using NUnit.Framework;
+using Smi.Common.Tests;
+using System;
+using System.Collections.Generic;
+
+namespace Microservices.CohortPackager.Tests.Execution.JobProcessing.Reporting
+{
+    public class FailureDataTests
+    {
+        #region Fixture Methods
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            TestLogger.Setup();
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown() { }
+
+        #endregion
+
+        #region Test Methods
+
+        [SetUp]
+        public void SetUp() { }
+
+        [TearDown]
+        public void TearDown() { }
+
+        #endregion
+
+        #region Tests
+
+        [Test]
+        public void Constructor_NullOrEmptyFailureParts_ThrowsException()
+        {
+            // Arrange
+
+            IEnumerable<FailurePart> parts = null;
+
+            // Act
+            // Assert
+
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                new FailureData(parts, "foo", "bar");
+            });
+
+            // Arrange
+
+            parts = new List<FailurePart>();
+
+            // Act
+            // Assert
+
+            Assert.Throws<ArgumentException>(() =>
+            {
+                new FailureData(parts, "foo", "bar");
+            });
+        }
+
+        [Test]
+        public void Constructor_NullOrWhitespaceProblemField_ThrowsException()
+        {
+            // Arrange
+
+            string problemField = null;
+
+            // Act
+            // Assert
+
+            Assert.Throws<ArgumentException>(() =>
+            {
+                new FailureData(new List<FailurePart>(), problemField, "bar");
+            });
+
+            // Arrange
+
+            problemField = "   ";
+
+            // Act
+            // Assert
+
+            Assert.Throws<ArgumentException>(() =>
+            {
+                new FailureData(new List<FailurePart>(), problemField, "bar");
+            });
+        }
+
+        [Test]
+        public void Constructor_NullOrWhitespaceProblemValue_ThrowsException()
+        {
+            // Arrange
+
+            string problemValue = null;
+
+            // Act
+            // Assert
+
+            Assert.Throws<ArgumentException>(() =>
+            {
+                new FailureData(new List<FailurePart>(), "foo", problemValue);
+            });
+
+            // Arrange
+
+            problemValue = "   ";
+
+            // Act
+            // Assert
+
+            Assert.Throws<ArgumentException>(() =>
+            {
+                new FailureData(new List<FailurePart>(), "foo", problemValue);
+            });
+        }
+
+        [Test]
+        public void FromFailure_HappyPath()
+        {
+            // Arrange
+
+            var parts = new List<FailurePart>()
+            {
+                new FailurePart("foo", FailureClassification.Person, 0),
+            };
+            var f = new Failure(parts)
+            {
+                Resource = "foo.dcm",
+                ResourcePrimaryKey = "1.2.3.4",
+                ProblemField = "TextValue",
+                ProblemValue = "foo bar",
+            };
+
+            // Act
+
+            var failureData = FailureData.FromFailure(f);
+
+            // Assert
+
+            Assert.AreEqual(1, failureData.Parts.Count);
+            Assert.AreEqual(new FailurePart("foo", FailureClassification.Person, 0), failureData.Parts[0]);
+            Assert.AreEqual("TextValue", failureData.ProblemField);
+            Assert.AreEqual("foo bar", failureData.ProblemValue);
+        }
+
+        #endregion
+    }
+}
