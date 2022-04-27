9c2a79c04ad6dcf2ddb1e70df05adfc0cab89da5
James A Sutherland <j.a.sutherland@dundee.ac.uk>
2022-04-27 13:25:18 +0000

    Adjust DB reachability test to ignore non-DB backends

--- tests/microservices/Microservices.DicomRelationalMapper.Tests/RunMeFirstTests/RunMeFirstRdmpTests.cs
+++ tests/microservices/Microservices.DicomRelationalMapper.Tests/RunMeFirstTests/RunMeFirstRdmpTests.cs
@@ -1,8 +1,6 @@
-﻿
-using FAnsi;
+﻿using FAnsi;
 using NUnit.Framework;
 using Smi.Common.Tests;
-using System;
 using System.IO;
 using MapsDirectlyToDatabaseTable;
 using Tests.Common;
@@ -21,16 +19,10 @@ namespace Microservices.DicomRelationalMapper.Tests.RunMeFirstTests
             if (!File.Exists(f))
                 Assert.Fail("TestDatabases.txt was not found in the bin directory, check the project includes a reference to HIC.RDMP.Plugin.Tests nuget package and that the file is set to CopyAlways");
 
-            Console.WriteLine("TestDatabases.txt");
-            Console.WriteLine("----------------------------------------------------------");
-            Console.Write(File.ReadAllText(f));
-
-            Exception ex;
-            if (CatalogueRepository is not ITableRepository crtr || !crtr.DiscoveredServer.RespondsWithinTime(5, out ex))
+            if (CatalogueRepository is ITableRepository crtr && !crtr.DiscoveredServer.RespondsWithinTime(5, out _))
                 Assert.Fail("Catalogue database was unreachable");
-            if (DataExportRepository is not ITableRepository dertr || !dertr.DiscoveredServer.RespondsWithinTime(5, out ex))
+            if (DataExportRepository is ITableRepository dertr && !dertr.DiscoveredServer.RespondsWithinTime(5, out _))
                 Assert.Fail("DataExport database was unreachable");
-
         }
     }
 }
