f3e248ea61e649dbc9fee781a81b421bd3fd683a
James A Sutherland <j@sutherland.pw>
2022-06-15 12:27:51 -0500

    Add archive support to DicomLoader

--- src/applications/Applications.DicomLoader/Loader.cs
+++ src/applications/Applications.DicomLoader/Loader.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections.Concurrent;
 using System.Diagnostics;
+using System.Text;
 using DicomTypeTranslation;
 using FellowOakDicom;
 using MongoDB.Bson;
 using MongoDB.Driver;
+using SharpCompress.Readers;
 using Smi.Common.Messages;
 using Smi.Common.MongoDB;
 
@@ -16,13 +18,13 @@ public static class Loader
     private static readonly ConcurrentDictionary<string,SeriesMessage> _seriesList=new ();
     private static readonly Stopwatch _timer = Stopwatch.StartNew();
 
-    private static SeriesMessage LoadSm(string id, FileInfo fi, DicomDataset ds, string studyId)
+    private static SeriesMessage LoadSm(string id, string directoryName, DicomDataset ds, string studyId)
     {
         // Try loading from Mongo in case we were interrupted previously
         var b=SeriesStore.Find(new BsonDocument("SeriesInstanceUID", id)).FirstOrDefault();
         return b ?? new SeriesMessage
         {
-            DirectoryPath = fi.DirectoryName,
+            DirectoryPath = directoryName,
             DicomDataset = DicomTypeTranslater.SerializeDatasetToJson(ds),
             ImagesInSeries = 1,
             SeriesInstanceUID = id,
@@ -53,7 +55,34 @@ public static class Loader
         if (_timer.ElapsedMilliseconds == 0) return;
         Console.WriteLine($"Processed {_fileCount} files in {_timer.ElapsedMilliseconds}ms ({1000*_fileCount/_timer.ElapsedMilliseconds} per second)");
     }
+
+    private static readonly byte[] _dicomMagic = Encoding.ASCII.GetBytes("DICM");
+
     private static void Process(FileInfo fi, IMongoCollection<BsonDocument> iStore, CancellationToken ct)
+    {
+        var bBuffer=new byte[132];
+        var buffer = new Span<byte>(bBuffer);
+        using var fileStream = File.OpenRead(fi.FullName);
+        if (fileStream.Read(buffer) == 132 && buffer[128..]==_dicomMagic)
+        {
+            var ds = DicomFile.Open(fileStream).Dataset;
+            Process(ds,fi.FullName,fi.DirectoryName ?? throw new ApplicationException($"Unable to find parent directory for {fi.FullName}"),fi.Length, iStore, ct);
+            return;
+        }
+        // Not DICOM? OK, try it as an archive:
+        using var archiveStream = File.OpenRead(fi.FullName);
+        using var archive=ReaderFactory.Open(archiveStream);
+        while(archive.MoveToNextEntry())
+        {
+            if (archive.Entry.IsDirectory) continue;
+            using var entry = archive.OpenEntryStream();
+            var ds = DicomFile.Open(entry).Dataset;
+            Process(ds,$"{fi.FullName}!{archive.Entry.Key}",fi.DirectoryName??throw new ApplicationException($"No parent directory for {fi.FullName}"),archive.Entry.Size,iStore,ct);
+        }
+    }
+
+    private static void Process(DicomDataset ds, string path, string directoryName, long size,
+        IMongoCollection<BsonDocument> iStore, CancellationToken ct)
     {
         // Consider flushing every 256 file loads
         if ((Interlocked.Increment(ref _fileCount) & 0xff) == 0)
@@ -61,7 +90,7 @@ public static class Loader
             Flush();
             Report();
         }
-        var ds = DicomFile.Open(fi.FullName).Dataset;
+
         var identifiers = new string[3];
 
         // Pre-fetch these to ensure they exist before we go further
@@ -71,7 +100,7 @@ public static class Loader
 
         if (identifiers.Any(string.IsNullOrWhiteSpace))
         {
-            Console.WriteLine($"'{fi.FullName}' had blank DICOM UID");
+            Console.WriteLine($"'{path}' had blank DICOM UID");
             return;
         }
 
@@ -81,17 +110,18 @@ public static class Loader
             SeriesInstanceUID = identifiers[1],
             SOPInstanceUID = identifiers[2],
 
-            DicomFileSize = fi.Length,
-            DicomFilePath = fi.FullName
+            DicomFileSize = size,
+            DicomFilePath = path
         };
         // ReSharper disable once InconsistentlySynchronizedField
-        _seriesList.AddOrUpdate(identifiers[1],id=>LoadSm(id,fi,ds,identifiers[0]) , IncSm);
+        _seriesList.AddOrUpdate(identifiers[1],id=>LoadSm(id,directoryName,ds,identifiers[0]) , IncSm);
         DicomDataset filtered = new(ds.Where(i => i is not DicomOtherByteFragment).ToArray());
 
         iStore.InsertOne(
             new BsonDocument("header", MongoDocumentHeaders.ImageDocumentHeader(message, new MessageHeader())).AddRange(
                 DicomTypeTranslaterReader.BuildBsonDocument(filtered)), cancellationToken: ct);
     }
+
     public static ValueTask Load(string filename, CancellationToken ct)
     {
         if (ImageStore is null) throw new ArgumentNullException(nameof(ImageStore));
