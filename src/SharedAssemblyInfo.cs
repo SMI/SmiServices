64ad86d0e49540b9dff782520a01da92ba9c300c
Thomas Nind <TZNind@dundee.ac.uk>
2022-06-22 12:15:42 +0100

    bump readme and SharedAssemblyInfo

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -7,6 +7,6 @@ using System.Reflection;
 [assembly: AssemblyCulture("")]
 
 // These should be overwritten by release builds
-[assembly: AssemblyVersion("5.0.1")]
-[assembly: AssemblyFileVersion("5.0.1")]
-[assembly: AssemblyInformationalVersion("5.0.1")] // This one can have the extra build info after it
+[assembly: AssemblyVersion("5.0.2")]
+[assembly: AssemblyFileVersion("5.0.2")]
+[assembly: AssemblyInformationalVersion("5.0.2")] // This one can have the extra build info after it
