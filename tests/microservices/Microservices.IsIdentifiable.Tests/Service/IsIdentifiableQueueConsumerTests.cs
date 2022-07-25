58e46b527b6cc4ae8e2a546d7eaa5a2c927ea361
Ruairidh MacLeod <ruairidh@rkm.dev>
2022-07-25 15:03:45 +0100

    wip - rebase me

---
+++ tests/microservices/Microservices.IsIdentifiable.Tests/Service/IsIdentifiableQueueConsumerTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using Smi.Common.Tests;
+
+namespace Microservices.IsIdentifiable.Tests.Service
+{
+    public class IsIdentifiableQueueConsumerTests
+    {
+        #region Fixture Methods
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            TestLogger.Setup();
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown() { }
+
+        #endregion
+
+        #region Test Methods
+
+        [SetUp]
+        public void SetUp() { }
+
+        [TearDown]
+        public void TearDown() { }
+
+        #endregion
+
+        #region Tests
+
+        [Test]
+        public void ProcessMessageImpl_HandlesFoo()
+        {
+
+        }
+
+        #endregion
+    }
+}
