68673b404acf55baf741e6943524c50e2ba59d3b
James A Sutherland <>
2023-02-06 17:15:24 -0600

    Remove timeout for copying between staging and live db tables

--- src/microservices/Microservices.DicomRelationalMapper/Execution/MigrateRawToStagingWithSelectIntoStatements.cs
+++ src/microservices/Microservices.DicomRelationalMapper/Execution/MigrateRawToStagingWithSelectIntoStatements.cs
@@ -17,7 +17,7 @@ namespace Microservices.DicomRelationalMapper.Execution
 {
     internal class MigrateRawToStagingWithSelectIntoStatements : DataLoadComponent
     {
-        
+
         public override ExitCodeType Run(IDataLoadJob job, GracefulCancellationToken cancellationToken)
         {
             if (Skip(job))
@@ -26,24 +26,21 @@ namespace Microservices.DicomRelationalMapper.Execution
             var configuration = job.Configuration;
             var namer = configuration.DatabaseNamer;
 
-            DiscoveredServer server = job.LoadMetadata.GetDistinctLiveDatabaseServer();
+            var server = job.LoadMetadata.GetDistinctLiveDatabaseServer();
 
             //Drop any STAGING tables that already exist
             foreach (var table in job.RegularTablesToLoad)
             {
-                string stagingDbName = table.GetDatabaseRuntimeName(LoadStage.AdjustStaging, namer);
-                string stagingTableName = table.GetRuntimeName(LoadStage.AdjustStaging, namer);
-                
+                var stagingDbName = table.GetDatabaseRuntimeName(LoadStage.AdjustStaging, namer);
+                var stagingTableName = table.GetRuntimeName(LoadStage.AdjustStaging, namer);
+
                 var stagingDb = server.ExpectDatabase(stagingDbName);
                 var stagingTable = stagingDb.ExpectTable(stagingTableName);
 
-                if (stagingDb.Exists())
+                if (stagingDb.Exists() && stagingTable.Exists())
                 {
-                    if (stagingTable.Exists())
-                    {
-                        job.OnNotify(this,new NotifyEventArgs(ProgressEventType.Information,$"Dropping existing STAGING table remnant {stagingTable.GetFullyQualifiedName()}"));
-                        stagingTable.Drop();
-                    }   
+                    job.OnNotify(this,new NotifyEventArgs(ProgressEventType.Information,$"Dropping existing STAGING table remnant {stagingTable.GetFullyQualifiedName()}"));
+                    stagingTable.Drop();
                 }
             }
 
@@ -52,20 +49,20 @@ namespace Microservices.DicomRelationalMapper.Execution
             job.CreateTablesInStage(cloner, LoadBubble.Staging);
 
 
-            using DbConnection con = server.GetConnection();
+            using var con = server.GetConnection();
             con.Open();
 
-            Stopwatch sw = Stopwatch.StartNew();
+            var sw = Stopwatch.StartNew();
 
             foreach (TableInfo table in job.RegularTablesToLoad)
             {
-                string fromDb = table.GetDatabaseRuntimeName(LoadStage.AdjustRaw, namer);
-                string toDb = table.GetDatabaseRuntimeName(LoadStage.AdjustStaging, namer);
+                var fromDb = table.GetDatabaseRuntimeName(LoadStage.AdjustRaw, namer);
+                var toDb = table.GetDatabaseRuntimeName(LoadStage.AdjustStaging, namer);
 
-                string fromTable = table.GetRuntimeName(LoadStage.AdjustRaw, namer);
-                string toTable = table.GetRuntimeName(LoadStage.AdjustStaging, namer);
+                var fromTable = table.GetRuntimeName(LoadStage.AdjustRaw, namer);
+                var toTable = table.GetRuntimeName(LoadStage.AdjustStaging, namer);
 
-                IQuerySyntaxHelper syntaxHelper = table.GetQuerySyntaxHelper();
+                var syntaxHelper = table.GetQuerySyntaxHelper();
 
                 var fromCols = server.ExpectDatabase(fromDb).ExpectTable(fromTable).DiscoverColumns();
                 var toCols = server.ExpectDatabase(toDb).ExpectTable(toTable).DiscoverColumns();
@@ -73,14 +70,13 @@ namespace Microservices.DicomRelationalMapper.Execution
                 //Migrate only columns that appear in both tables
                 var commonColumns = fromCols.Select(f => f.GetRuntimeName()).Intersect(toCols.Select(t => t.GetRuntimeName())).ToArray();
 
-                string sql = string.Format(@"INSERT INTO {1}({2}) SELECT DISTINCT {2} FROM {0}",
-                    syntaxHelper.EnsureFullyQualified(fromDb, null, fromTable),
-                    syntaxHelper.EnsureFullyQualified(toDb, null, toTable),
-                    string.Join(",",commonColumns.Select(c=>syntaxHelper.EnsureWrapped(c))));
+                var sql =
+                    $@"INSERT INTO {syntaxHelper.EnsureFullyQualified(toDb, null, toTable)}({string.Join(",", commonColumns.Select(c => syntaxHelper.EnsureWrapped(c)))}) SELECT DISTINCT {string.Join(",", commonColumns.Select(c => syntaxHelper.EnsureWrapped(c)))} FROM {syntaxHelper.EnsureFullyQualified(fromDb, null, fromTable)}";
 
                 job.OnNotify(this, new NotifyEventArgs(ProgressEventType.Information, "About to send SQL:" + sql));
 
-                DbCommand cmd = server.GetCommand(sql, con);
+                var cmd = server.GetCommand(sql, con);
+                cmd.CommandTimeout = 0;
 
                 try
                 {
@@ -94,7 +90,7 @@ namespace Microservices.DicomRelationalMapper.Execution
             }
 
             sw.Stop();
-                
+
             job.OnNotify(this, new NotifyEventArgs(ProgressEventType.Information, "Migrated all rows using INSERT INTO in " + sw.ElapsedMilliseconds + "ms"));
             return ExitCodeType.Success;
         }
