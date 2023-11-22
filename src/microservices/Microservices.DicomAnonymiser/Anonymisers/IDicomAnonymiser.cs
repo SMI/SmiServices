4edd39dc9503c097fb2d7f17e7beb4df3364ca5e
darshad <d.arshad@epcc.ed.ac.uk>
2023-11-22 14:17:00 +0000

    Update IDicomAnonymiser interface to include ExtractFileMessage parameter
    This commit modifies the Anonymise method in the IDicomAnonymiser interface to include an additional ExtractFileMessage parameter. This change allows the method to use the message's modality when anonymising the specified source file to the destination file.

--- src/microservices/Microservices.DicomAnonymiser/Anonymisers/IDicomAnonymiser.cs
+++ src/microservices/Microservices.DicomAnonymiser/Anonymisers/IDicomAnonymiser.cs
@@ -6,12 +6,15 @@ namespace Microservices.DicomAnonymiser.Anonymisers
     public interface IDicomAnonymiser
     {
         /// <summary>
-        /// Anonymise the specified <paramref name="sourceFile"/> to <paramref name="destFile"></paramref>.
+        /// Anonymise the specified <paramref name="sourceFile"/> to <paramref name="destFile"></paramref> based on the provided <paramref name="message"/> modality.
         /// Implementations should assume that <paramref name="sourceFile"/> already exists, and <paramref name="destFile"></paramref> does not exist. 
         /// </summary>
+        /// <param name="message"></param>
         /// <param name="sourceFile"></param>
         /// <param name="destFile"></param>
         /// <returns></returns>
-        ExtractedFileStatus Anonymise(IFileInfo sourceFile, IFileInfo destFile);
+        // ExtractedFileStatus Anonymise(IFileInfo sourceFile, IFileInfo destFile);
+
+        ExtractedFileStatus Anonymise(ExtractFileMessage message, IFileInfo sourceFile, IFileInfo destFile);
     }
 }
