f431ae471697178dd4a70e51906399921be831df
James A Sutherland <>
2023-12-12 10:56:38 -0600

    Fix install script issues from JIRA RDMP-78

--- docs/install.sh
+++ docs/install.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-mkdir -p /imaging/{bin,conf/rdmp,data} /imaging/db/{mongo,sql} 
+mkdir -p /imaging/{bin/rdmp,bin/smi,conf/rdmp,data} /imaging/db/{mongo,sql} 
 curl -sL https://github.com/HicServices/RDMP/releases/download/v8.0.7/rdmp-8.0.7-cli-linux-x64.tar.xz | tar x --strip-components=1 -JC /imaging/bin/rdmp -f - 
 curl -sL https://github.com/SMI/SmiServices/releases/download/v5.3.0/smi-services-v5.3.0-linux-x64.tgz | tar x --strip-components=1 -zC /imaging/bin/smi -f - 
-mysql --uroot --password=mysqlrootpassword < schema.sql
+mysql --user=root --password=mysqlrootpassword < schema.sql
 wget https://raw.githubusercontent.com/HicServices/DicomTypeTranslation/main/Templates/CT.it 
-/imaging/bin/rdmp/rdmp createnew 
-externaldatabaseserver LiveLoggingServer_ID "DatabaseType:MySQL:Server=127.0.0.1;Uid=smi;Pwd=SmiSqlPassword;Database=smi" --dir /imaging/conf/rdmp 
+/imaging/bin/rdmp/rdmp createnew externaldatabaseserver LiveLoggingServer_ID "DatabaseType:MySQL:Server=127.0.0.1;Uid=smi;Pwd=SmiSqlPassword;Database=smi" --dir /imaging/conf/rdmp 
 /imaging/bin/rdmp/rdmp cmd CreateNewImagingDatasetSuite "DatabaseType:MySQL:Name:smi:Server=127.0.0.1;Uid=smi;Pwd=SmiSqlPassword;Database=smi_images" ./data DicomFileCollectionSource CT_ CT.it false true --dir /imaging/conf/rdmp 
 for modality in CR CT DX IO MG MR NM OTHER PT PX RF SR US XA 
 do 
-sed –e "s/MR_/${modality}_/g" <<'EOC' | ~/bin/rdmp/rdmp -f /dev/stdin --dir /imaging/conf/rdmp 
+sed –e "s/MR_/${modality}_/g" <<'EOC' | /imaging/bin/rdmp/rdmp -f /dev/stdin --dir /imaging/conf/rdmp
 Commands: 
         - newobject joininfo columninfo:*MR_*ImageTable`*Series*UID* columninfo:*MR_*SeriesTable`*Series*UID* inner null 
         - newobject joininfo columninfo:*MR_*SeriesTable`*Study*UID* columninfo:*MR_*StudyTable`*Study*UID* inner null 
