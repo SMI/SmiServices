f4a70dffa1aef690ea1738a2baa9ca94783819c3
James A Sutherland <j@sutherland.pw>
2024-08-21 06:54:34 -0500

    Update QueryToExecuteResult.cs
    Make QueryToExecuteResult a record type

--- src/SmiServices/Microservices/CohortExtractor/RequestFulfillers/QueryToExecuteResult.cs
+++ src/SmiServices/Microservices/CohortExtractor/RequestFulfillers/QueryToExecuteResult.cs
@@ -1,42 +1,12 @@
-using Equ;
-using System;
+namespace SmiServices.Microservices.CohortExtractor.RequestFulfillers;
 
-
-namespace SmiServices.Microservices.CohortExtractor.RequestFulfillers
+public sealed record QueryToExecuteResult(
+    string FilePathValue,
+    string? StudyTagValue,
+    string? SeriesTagValue,
+    string? InstanceTagValue,
+    bool Reject,
+    string? RejectReason)
 {
-    public class QueryToExecuteResult : MemberwiseEquatable<QueryToExecuteResult>
-    {
-        public readonly string FilePathValue;
-        public readonly string? StudyTagValue;
-        public readonly string? SeriesTagValue;
-        public readonly string? InstanceTagValue;
-
-        public readonly bool Reject;
-        public readonly string? RejectReason;
-
-        public QueryToExecuteResult(
-            string filePathValue,
-            string? studyTagValue,
-            string? seriesTagValue,
-            string? instanceTagValue,
-            bool rejection,
-            string? rejectionReason
-        )
-        {
-            FilePathValue = filePathValue;
-            StudyTagValue = studyTagValue;
-            SeriesTagValue = seriesTagValue;
-            InstanceTagValue = instanceTagValue;
-            Reject = rejection;
-            RejectReason = rejectionReason;
-
-            if (Reject && string.IsNullOrWhiteSpace(RejectReason))
-                throw new ArgumentException("RejectReason must be specified if Reject=true");
-        }
-
-        public override string ToString()
-        {
-            return $"{FilePathValue}(Reject={Reject})";
-        }
-    }
+    public override string ToString() => $"{FilePathValue}(Reject={Reject})";
 }
