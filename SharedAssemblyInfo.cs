1a937d03ac1d994d2593049e1cd54bebe7f9912b
Ruairidh MacLeod <ruairidh@rkm.dev>
2024-09-19 20:04:34 +0100

    bump version for release

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -7,6 +7,6 @@ using System.Reflection;
 [assembly: AssemblyCulture("")]
 
 // These should be overwritten by release builds
-[assembly: AssemblyVersion("5.7.2")]
-[assembly: AssemblyFileVersion("5.7.2")]
-[assembly: AssemblyInformationalVersion("5.7.2")] // This one can have the extra build info after it
+[assembly: AssemblyVersion("5.8.0")]
+[assembly: AssemblyFileVersion("5.8.0")]
+[assembly: AssemblyInformationalVersion("5.8.0")] // This one can have the extra build info after it
