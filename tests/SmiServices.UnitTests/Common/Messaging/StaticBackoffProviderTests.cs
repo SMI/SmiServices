456204a92ec01b80990bce952227df7d5b1ba900
Ruairidh MacLeod <ruairidh@rkm.dev>
2024-09-26 16:56:19 +0100

    add tests for coverage

--- tests/SmiServices.UnitTests/Common/Messaging/StaticBackoffProviderTests.cs
+++ tests/SmiServices.UnitTests/Common/Messaging/StaticBackoffProviderTests.cs
@@ -12,11 +12,19 @@ internal class StaticBackoffProviderTests
     }
 
     [Test]
-    public void Constructor_TimeSpan()
+    public void Constructor_WithTimeSpan_IsSet()
     {
         var provider = new StaticBackoffProvider(new TimeSpan(1, 2, 3));
 
         Assert.That(provider.GetNextBackoff(), Is.EqualTo(new TimeSpan(1, 2, 3)));
+    }
+
+    [Test]
+    public void Constructor_WithNoTimeSpan_UsesDefault()
+    {
+        var provider = new StaticBackoffProvider();
+
+        Assert.That(provider.GetNextBackoff(), Is.EqualTo(new TimeSpan(0, 1, 0)));
     }
 
     [Test]
