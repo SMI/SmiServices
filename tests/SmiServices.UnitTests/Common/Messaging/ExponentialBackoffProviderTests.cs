6d5a1e357463ab1ae0fecbbf0f94940a1f733811
Ruairidh MacLeod <ruairidh@rkm.dev>
2024-09-26 16:56:19 +0100

    add tests for coverage

--- tests/SmiServices.UnitTests/Common/Messaging/ExponentialBackoffProviderTests.cs
+++ tests/SmiServices.UnitTests/Common/Messaging/ExponentialBackoffProviderTests.cs
@@ -13,6 +13,22 @@ internal class ExponentialBackoffProviderTests
     }
 
     [Test]
+    public void Constructor_WithTimeSpan_IsSet()
+    {
+        var provider = new ExponentialBackoffProvider(new TimeSpan(1, 2, 3));
+
+        Assert.That(provider.GetNextBackoff(), Is.EqualTo(new TimeSpan(1, 2, 3)));
+    }
+
+    [Test]
+    public void Constructor_WithNoTimeSpan_UsesDefault()
+    {
+        var provider = new ExponentialBackoffProvider();
+
+        Assert.That(provider.GetNextBackoff(), Is.EqualTo(new TimeSpan(0, 1, 0)));
+    }
+
+    [Test]
     public void GetNextBackoff_ReturnsIncreasingTimeSpan()
     {
         var provider = new ExponentialBackoffProvider(new TimeSpan(1));
