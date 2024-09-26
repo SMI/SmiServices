6046880f213c5a9773da79841c5689b418e7674b
Ruairidh MacLeod <ruairidh@rkm.dev>
2024-09-26 13:53:47 +0100

    add basic unit tests for ProducerModel.SendMessage

---
+++ tests/SmiServices.UnitTests/Common/Messaging/ProducerModelTests.cs
@@ -0,0 +1,62 @@
+using Moq;
+using NUnit.Framework;
+using RabbitMQ.Client;
+using SmiServices.Common.Messages;
+using SmiServices.Common.Messaging;
+using System;
+using System.Collections.Generic;
+
+namespace SmiServices.UnitTests.Common.Messaging;
+
+internal class ProducerModelTests
+{
+    private class TestMessage : IMessage { }
+
+    [OneTimeSetUp]
+    public void OneTimeSetUp()
+    {
+        TestLogger.Setup();
+    }
+
+    [Test]
+    public void SendMessage_HappyPath()
+    {
+        // Arrange
+        bool timedOut = false;
+        var mockModel = new Mock<IModel>(MockBehavior.Strict);
+        mockModel.Setup(x => x.BasicPublish("Exchange", "", true, It.IsAny<IBasicProperties>(), It.IsAny<ReadOnlyMemory<byte>>()));
+        mockModel.Setup(x => x.WaitForConfirms(It.IsAny<TimeSpan>(), out timedOut)).Returns(true);
+
+        var mockBasicProperties = new Mock<IBasicProperties>();
+        mockBasicProperties.Setup(x => x.Headers).Returns(new Dictionary<string, object>());
+
+        var producerModel = new ProducerModel("Exchange", mockModel.Object, mockBasicProperties.Object);
+        var message = new TestMessage();
+
+        // Act
+        // Assert
+        Assert.DoesNotThrow(() => producerModel.SendMessage(message, inResponseTo: null, routingKey: null));
+    }
+
+    [Test]
+    public void SendMessage_ThrowsException_OnTimeout()
+    {
+        // Arrange
+        bool timedOut = true;
+        var mockModel = new Mock<IModel>(MockBehavior.Strict);
+        mockModel.Setup(x => x.BasicPublish("Exchange", "", true, It.IsAny<IBasicProperties>(), It.IsAny<ReadOnlyMemory<byte>>()));
+        mockModel.Setup(x => x.WaitForConfirms(It.IsAny<TimeSpan>(), out timedOut)).Returns(false);
+
+        var mockBasicProperties = new Mock<IBasicProperties>();
+        mockBasicProperties.Setup(x => x.Headers).Returns(new Dictionary<string, object>());
+
+        var maxRetryAttempts = 1;
+        var producerModel = new ProducerModel("Exchange", mockModel.Object, mockBasicProperties.Object, maxRetryAttempts);
+        var message = new TestMessage();
+
+        // Act
+        // Assert
+        var exc = Assert.Throws<ApplicationException>(() => producerModel.SendMessage(message, inResponseTo: null, routingKey: null));
+        Assert.That(exc.Message, Is.EqualTo("Could not confirm message published after timeout"));
+    }
+}
