865228d15c85c441b4b7cd11d8e1778c6a2342fc
Ruairidh MacLeod <ruairidh@rkm.dev>
2024-10-04 16:27:13 +0100

    add tests for StudySeriesSOPProjectPathResolver

---
+++ tests/SmiServices.UnitTests/Microservices/CohortExtractor/StudySeriesSOPProjectPathResolverTests.cs
@@ -0,0 +1,133 @@
+using NUnit.Framework;
+using SmiServices.Common.Messages.Extraction;
+using SmiServices.Microservices.CohortExtractor.ProjectPathResolvers;
+using SmiServices.Microservices.CohortExtractor.RequestFulfillers;
+using SmiServices.UnitTests.Common;
+using System.IO.Abstractions;
+using System.IO.Abstractions.TestingHelpers;
+
+
+namespace SmiServices.UnitTests.Microservices.CohortExtractor
+{
+    public class StudySeriesSOPProjectPathResolverTests
+    {
+        #region Fixture Methods
+
+        private ExtractionRequestMessage _requestMessage = null!;
+        private IFileSystem _fileSystem;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            TestLogger.Setup();
+
+            _requestMessage = new ExtractionRequestMessage
+            {
+                IsIdentifiableExtraction = false,
+            };
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown() { }
+
+        #endregion
+
+        #region Test Methods
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fileSystem = new MockFileSystem();
+        }
+
+        [TearDown]
+        public void TearDown() { }
+
+        #endregion
+
+        #region Tests
+
+        [Test]
+        public void GetOutputPath_Basic()
+        {
+            // Arrange
+
+            var expectedPath = _fileSystem.Path.Combine("study", "series", "sop-an.dcm");
+            var resolver = new StudySeriesSOPProjectPathResolver(_fileSystem);
+            var result = new QueryToExecuteResult(
+                "foo.dcm",
+                "study",
+                "series",
+                "sop",
+                rejection: false,
+                rejectionReason: null
+            );
+            var message = new ExtractionRequestMessage();
+
+            // Act
+
+            var actualPath = resolver.GetOutputPath(result, message);
+
+            // Assert
+            Assert.That(actualPath, Is.EqualTo(expectedPath));
+        }
+
+        [TestCase("file.dcm")]
+        [TestCase("file.dicom")]
+        [TestCase("file")]
+        [TestCase("file.foo")]
+        public void GetOutputPath_Extensions(string inputFile)
+        {
+            // Arrange
+
+            var expectedPath = _fileSystem.Path.Combine("study", "series", "sop-an.dcm");
+            var resolver = new StudySeriesSOPProjectPathResolver(_fileSystem);
+            var result = new QueryToExecuteResult(
+                inputFile,
+                "study",
+                "series",
+                "sop",
+                rejection: false,
+                rejectionReason: null
+            );
+            var message = new ExtractionRequestMessage();
+
+            // Act
+
+            var actualPath = resolver.GetOutputPath(result, message);
+
+            // Assert
+            Assert.That(actualPath, Is.EqualTo(expectedPath));
+        }
+
+        [Test]
+        public void GetOutputPath_IdentExtraction()
+        {
+            // Arrange
+
+            var expectedPath = _fileSystem.Path.Combine("study", "series", "sop.dcm");
+            var resolver = new StudySeriesSOPProjectPathResolver(_fileSystem);
+            var result = new QueryToExecuteResult(
+                "foo.dcm",
+                "study",
+                "series",
+                "sop",
+                rejection: false,
+                rejectionReason: null
+            );
+            var message = new ExtractionRequestMessage()
+            {
+                IsIdentifiableExtraction = true,
+            };
+
+            // Act
+
+            var actualPath = resolver.GetOutputPath(result, message);
+
+            // Assert
+            Assert.That(actualPath, Is.EqualTo(expectedPath));
+        }
+
+        #endregion
+    }
+}
