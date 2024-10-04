1bdafbb0fbccf192177b3f78bf89062da24923b5
Ruairidh MacLeod <ruairidh@rkm.dev>
2024-10-04 15:52:46 +0100

    add IFileSystem and extract constants

---
+++ src/SmiServices/Microservices/CohortExtractor/ProjectPathResolvers/ProjectPathResolverConstants.cs
@@ -0,0 +1,13 @@
+namespace SmiServices.Microservices.CohortExtractor.ProjectPathResolvers;
+public static class ProjectPathResolverConstants
+{
+    /// <summary>
+    /// Extension used when creating anonymised files
+    /// </summary>
+    public const string ANON_EXT = "-an.dcm";
+
+    /// <summary>
+    /// Extension used when creating "identifiable" files
+    /// </summary>
+    public const string IDENT_EXT = ".dcm";
+}
