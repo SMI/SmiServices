dfd38fd3f6eda05b7aef3c9787161a126d0c1c5c
Ruairidh MacLeod <ruairidh@rkm.dev>
2024-12-09 17:28:28 +0000

    add missing modality

--- src/microservices/com.smi.microservices.ctpanonymiser/src/test/java/org/smi/ctpanonymiser/test/messages/ExtractFileMessageTest.java
+++ src/microservices/com.smi.microservices.ctpanonymiser/src/test/java/org/smi/ctpanonymiser/test/messages/ExtractFileMessageTest.java
@@ -37,6 +37,7 @@ public class ExtractFileMessageTest extends TestCase {
 		_exMessage.ExtractionJobIdentifier = UUID.randomUUID();
 		_exMessage.JobSubmittedAt = "";
 		_exMessage.ExtractionDirectory = "dummyProjectDir/dummyExtractionDir";
+		_exMessage.Modality = "CT";
 		_exMessage.DicomFilePath = "2018/01/01/ABCD/image-000001.dcm";
 		_exMessage.OutputPath = "dummySeries/image-000001-an.dcm";
 		_exMessage.ProjectNumber = "1234-5678";
