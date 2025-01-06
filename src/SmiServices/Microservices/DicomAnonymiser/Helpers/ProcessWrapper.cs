621546d45101c52be23b21590f25c2d61d7e58fc
Ruairidh MacLeod <ruairidh@rkm.dev>
2025-01-06 14:06:43 +0000

    fix namespace block

--- src/SmiServices/Microservices/DicomAnonymiser/Helpers/ProcessWrapper.cs
+++ src/SmiServices/Microservices/DicomAnonymiser/Helpers/ProcessWrapper.cs
@@ -1,36 +1,35 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 
-namespace SmiServices.Microservices.DicomAnonymiser.Helpers
+namespace SmiServices.Microservices.DicomAnonymiser.Helpers;
+
+internal static class ProcessWrapper
 {
-    internal static class ProcessWrapper
+    public static Process CreateProcess(
+        string fileName,
+        string? arguments,
+        string? workingDirectory = null,
+        Dictionary<string, string>? environmentVariables = null
+    )
     {
-        public static Process CreateProcess(
-            string fileName,
-            string? arguments,
-            string? workingDirectory = null,
-            Dictionary<string, string>? environmentVariables = null
-        )
+        var process = new Process
         {
-            var process = new Process
+            StartInfo = new ProcessStartInfo
             {
-                StartInfo = new ProcessStartInfo
-                {
-                    FileName = fileName,
-                    Arguments = arguments,
-                    UseShellExecute = false,
-                    RedirectStandardInput = true,
-                    RedirectStandardOutput = true,
-                    RedirectStandardError = true,
-                    WorkingDirectory = workingDirectory ?? string.Empty
-                }
-            };
+                FileName = fileName,
+                Arguments = arguments,
+                UseShellExecute = false,
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                WorkingDirectory = workingDirectory ?? string.Empty,
+            }
+        };
 
-            if (environmentVariables != null)
-                foreach (var variable in environmentVariables)
-                    process.StartInfo.EnvironmentVariables[variable.Key] = variable.Value;
+        if (environmentVariables != null)
+            foreach (var variable in environmentVariables)
+                process.StartInfo.EnvironmentVariables[variable.Key] = variable.Value;
 
-            return process;
-        }
+        return process;
     }
 }
