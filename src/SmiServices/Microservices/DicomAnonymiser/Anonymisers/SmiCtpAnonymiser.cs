abc8ceccf484a20476af725c5f51fdcda62298f6
Ruairidh MacLeod <ruairidh@rkm.dev>
2025-01-06 12:15:45 +0000

    fix namespace block

--- src/SmiServices/Microservices/DicomAnonymiser/Anonymisers/SmiCtpAnonymiser.cs
+++ src/SmiServices/Microservices/DicomAnonymiser/Anonymisers/SmiCtpAnonymiser.cs
@@ -7,71 +7,70 @@ using System.Diagnostics;
 using System.IO;
 using System.Threading.Tasks;
 
-namespace SmiServices.Microservices.DicomAnonymiser.Anonymisers
+namespace SmiServices.Microservices.DicomAnonymiser.Anonymisers;
+
+internal class SmiCtpAnonymiser : IDicomAnonymiser, IDisposable
 {
-    internal class SmiCtpAnonymiser : IDicomAnonymiser, IDisposable
+    private readonly ILogger _logger = LogManager.GetCurrentClassLogger();
+    private readonly Process _ctpProcess;
+
+    public SmiCtpAnonymiser(GlobalOptions globalOptions)
     {
-        private readonly ILogger _logger = LogManager.GetCurrentClassLogger();
-        private readonly Process _ctpProcess;
+        var dicomAnonymiserOptions = globalOptions.DicomAnonymiserOptions ?? throw new ArgumentException($"{nameof(globalOptions.DicomAnonymiserOptions)} was null", nameof(globalOptions));
 
-        public SmiCtpAnonymiser(GlobalOptions globalOptions)
+        if (!File.Exists(dicomAnonymiserOptions.CtpAnonCliJar))
+            throw new ArgumentException($"{nameof(dicomAnonymiserOptions.CtpAnonCliJar)} '{dicomAnonymiserOptions.CtpAnonCliJar}' does not exist", nameof(globalOptions));
+
+        if (!File.Exists(dicomAnonymiserOptions.CtpAllowlistScript))
+            throw new ArgumentException($"{nameof(dicomAnonymiserOptions.CtpAllowlistScript)} '{dicomAnonymiserOptions.CtpAllowlistScript}' does not exist", nameof(globalOptions));
+
+        var srAnonTool = "false";
+        if (!string.IsNullOrWhiteSpace(dicomAnonymiserOptions.SRAnonymiserToolPath))
         {
-            var dicomAnonymiserOptions = globalOptions.DicomAnonymiserOptions ?? throw new ArgumentException($"{nameof(globalOptions.DicomAnonymiserOptions)} was null", nameof(globalOptions));
-
-            if (!File.Exists(dicomAnonymiserOptions.CtpAnonCliJar))
-                throw new ArgumentException($"{nameof(dicomAnonymiserOptions.CtpAnonCliJar)} '{dicomAnonymiserOptions.CtpAnonCliJar}' does not exist", nameof(globalOptions));
-
-            if (!File.Exists(dicomAnonymiserOptions.CtpAllowlistScript))
-                throw new ArgumentException($"{nameof(dicomAnonymiserOptions.CtpAllowlistScript)} '{dicomAnonymiserOptions.CtpAllowlistScript}' does not exist", nameof(globalOptions));
-
-            var srAnonTool = "false";
-            if (!string.IsNullOrWhiteSpace(dicomAnonymiserOptions.SRAnonymiserToolPath))
-            {
-                if (!File.Exists(dicomAnonymiserOptions.SRAnonymiserToolPath))
-                    throw new ArgumentException($"{nameof(dicomAnonymiserOptions.SRAnonymiserToolPath)} '{dicomAnonymiserOptions.SRAnonymiserToolPath}' does not exist", nameof(globalOptions));
-                srAnonTool = dicomAnonymiserOptions.SRAnonymiserToolPath;
-            }
-
-            var ctpArgs = $"-jar {dicomAnonymiserOptions.CtpAnonCliJar} --anon-script {dicomAnonymiserOptions.CtpAllowlistScript} --sr-anon-tool {srAnonTool} --daemonize";
-            _ctpProcess = ProcessWrapper.CreateProcess("java", ctpArgs);
-            _ctpProcess.Start();
-
-            var readyTask = Task.Run(() =>
-            {
-                while (_ctpProcess.StandardOutput.ReadLine() != "READY") { }
-            });
-            if (!readyTask.Wait(TimeSpan.FromSeconds(5)))
-                throw new Exception("Did not receive READY before timeout");
+            if (!File.Exists(dicomAnonymiserOptions.SRAnonymiserToolPath))
+                throw new ArgumentException($"{nameof(dicomAnonymiserOptions.SRAnonymiserToolPath)} '{dicomAnonymiserOptions.SRAnonymiserToolPath}' does not exist", nameof(globalOptions));
+            srAnonTool = dicomAnonymiserOptions.SRAnonymiserToolPath;
         }
 
-        public ExtractedFileStatus Anonymise(FileInfo sourceFile, FileInfo destFile, string modality, out string? anonymiserStatusMessage)
+        var ctpArgs = $"-jar {dicomAnonymiserOptions.CtpAnonCliJar} --anon-script {dicomAnonymiserOptions.CtpAllowlistScript} --sr-anon-tool {srAnonTool} --daemonize";
+        _ctpProcess = ProcessWrapper.CreateProcess("java", ctpArgs);
+        _ctpProcess.Start();
+
+        var readyTask = Task.Run(() =>
         {
-            _logger.Info($"{sourceFile} to {destFile}");
+            while (_ctpProcess.StandardOutput.ReadLine() != "READY") { }
+        });
+        if (!readyTask.Wait(TimeSpan.FromSeconds(5)))
+            throw new Exception("Did not receive READY before timeout");
+    }
 
-            var args = $"{sourceFile.FullName} {destFile.FullName}";
-            _ctpProcess.StandardInput.WriteLine(args);
-            var result = _ctpProcess.StandardOutput.ReadLine();
-            _logger.Info(result);
+    public ExtractedFileStatus Anonymise(FileInfo sourceFile, FileInfo destFile, string modality, out string? anonymiserStatusMessage)
+    {
+        _logger.Info($"{sourceFile} to {destFile}");
 
-            ExtractedFileStatus status;
-            if (result == "OK")
-            {
-                anonymiserStatusMessage = null;
-                status = ExtractedFileStatus.Anonymised;
-            }
-            else
-            {
-                anonymiserStatusMessage = result;
-                status = ExtractedFileStatus.ErrorWontRetry;
-            }
+        var args = $"{sourceFile.FullName} {destFile.FullName}";
+        _ctpProcess.StandardInput.WriteLine(args);
+        var result = _ctpProcess.StandardOutput.ReadLine();
+        _logger.Info(result);
 
-            return status;
+        ExtractedFileStatus status;
+        if (result == "OK")
+        {
+            anonymiserStatusMessage = null;
+            status = ExtractedFileStatus.Anonymised;
+        }
+        else
+        {
+            anonymiserStatusMessage = result;
+            status = ExtractedFileStatus.ErrorWontRetry;
         }
 
-        public void Dispose()
-        {
-            GC.SuppressFinalize(this);
-            _ctpProcess.Dispose();
-        }
+        return status;
+    }
+
+    public void Dispose()
+    {
+        GC.SuppressFinalize(this);
+        _ctpProcess.Dispose();
     }
 }
