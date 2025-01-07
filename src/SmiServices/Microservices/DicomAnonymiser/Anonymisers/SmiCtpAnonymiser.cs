c74da021c58cb9a37ff4e342ce7e347d602083aa
Ruairidh MacLeod <ruairidh@rkm.dev>
2025-01-07 14:39:02 +0000

    rework timeout logic

--- src/SmiServices/Microservices/DicomAnonymiser/Anonymisers/SmiCtpAnonymiser.cs
+++ src/SmiServices/Microservices/DicomAnonymiser/Anonymisers/SmiCtpAnonymiser.cs
@@ -6,6 +6,7 @@ using System;
 using System.Diagnostics;
 using System.IO;
 using System.IO.Abstractions;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SmiServices.Microservices.DicomAnonymiser.Anonymisers;
@@ -38,12 +39,27 @@ public class SmiCtpAnonymiser : IDicomAnonymiser, IDisposable
         _ctpProcess = ProcessWrapper.CreateProcess("java", ctpArgs);
         _ctpProcess.Start();
 
+        CancellationTokenSource ts = new();
+        var ready = false;
         var readyTask = Task.Run(() =>
         {
-            while (_ctpProcess.StandardOutput.ReadLine() != "READY") { }
+            string? line;
+            do
+            {
+                line = _ctpProcess.StandardOutput.ReadLine();
+                _logger.Debug(line);
+                if (line == "READY")
+                {
+                    ready = true;
+                    break;
+                }
+            }
+            while (!ts.IsCancellationRequested && line != null);
         });
-        if (!readyTask.Wait(TimeSpan.FromSeconds(5)))
+
+        if (!readyTask.Wait(TimeSpan.FromSeconds(5)) || !ready)
         {
+            ts.Cancel();
             _ctpProcess.Kill();
             throw new Exception($"Did not receive READY before timeout. Stderr: {_ctpProcess.StandardError.ReadToEnd()}");
         }
