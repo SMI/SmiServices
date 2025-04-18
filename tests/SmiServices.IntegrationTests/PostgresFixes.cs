18a64617ed7d79f3ee83f4aa478a667008fb0745
Ruairidh MacLeod <ruairidh@rkm.dev>
2025-04-18 18:11:52 +0100

    test

--- tests/SmiServices.IntegrationTests/PostgresFixes.cs
+++ tests/SmiServices.IntegrationTests/PostgresFixes.cs
@@ -1,20 +1,37 @@
-using FAnsi;
 using FAnsi.Discovery;
+using FAnsi;
+using NUnit.Framework;
+using Npgsql;
 using Tests.Common;
 
 namespace SmiServices.IntegrationTests;
 
 internal static class PostgresFixes
 {
+    /// <summary>
+    /// RDMP DatabaseTests does not currently work with Postgres due to a bug in FAnsi with checking server existence
+    /// on the non-default "postgres" database
+    /// </summary>
+    /// <param name="settings"></param>
+    /// <param name="type"></param>
     public static void GetCleanedServerPostgresFix(TestDatabasesSettings settings, DatabaseType type)
     {
         if (type != DatabaseType.PostgreSql)
             return;
 
         var server = new DiscoveredServer(settings.PostgreSql, DatabaseType.PostgreSql);
-        if (server.GetCurrentDatabase() != null)
-            return;
+        var scratchDatabaseName = TestDatabaseNames.GetConsistentName("ScratchArea");
 
-        server.ChangeDatabase("postgres");
+        try
+        {
+            server.CreateDatabase(scratchDatabaseName);
+        }
+        catch (PostgresException e)
+        {
+            if(!e.Message.Contains("already exists"))
+                throw;
+        }
+
+        Assert.That(server.ExpectDatabase(scratchDatabaseName).Exists(), Is.True);
     }
 }
